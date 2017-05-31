c1a9c4230d7836025b18ca116f375d8e0adc81d1
dkrnl <dkrnl@yandex.ru>
2017-05-31 16:56:19 +0700

    add browserlist

--- postcss.config.js
+++ postcss.config.js
@@ -2,26 +2,14 @@ const PROD = process.argv.indexOf("-p") !== -1;
 
 module.exports = {
     plugins: [
-        require("autoprefixer")({
-            browsers: [
-                "Android 2.3",
-                "Android >= 4",
-                "Chrome >= 20",
-                "Firefox >= 24",
-                "Explorer >= 8",
-                "iOS >= 6",
-                "Opera >= 12",
-                "Safari >= 6",
-            ],
-            add: true,
+        require("autoprefixer"),
+        require("cssnano")({
+            discardComments: { removeAll: PROD },
         }),
-       require("cssnano")({
-           discardComments: { removeAll: PROD },
-       }),
-       require("stylelint")({
-       }),
-       require("css-mqpacker")({
-           sort: true,
-       }),
+        require("stylelint")({
+        }),
+        require("css-mqpacker")({
+            sort: true,
+        }),
     ]
 };
