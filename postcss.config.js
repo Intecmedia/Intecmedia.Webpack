9b2a67f1f387ce63ae5bbb03e4d2a59f21b870cc
dkrnl <dkrnl@yandex.ru>
2017-06-05 12:06:53 +0700

    remove browserslistrc

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,4 @@
-const browserslist = require("browserslist");
 const PROD = process.argv.indexOf("-p") !== -1;
-
-const browsers = browserslist.readConfig("./.browserslistrc").defaults;
-
 
 module.exports = {
     sourceMap: !PROD,
@@ -12,14 +8,12 @@ module.exports = {
         require("postcss-focus")(),
         require("pixrem")({
             rootValue: 16,
-            browsers: browsers,
             unitPrecision: 3,
         }),
         require("css-mqpacker")({
             sort: true,
         }),
         require("autoprefixer")({
-            browsers: browsers,
             add: true,
         }),
         require("cssnano")({
