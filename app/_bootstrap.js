17ad352dd711b60f7a3ef66a92814d07130d213d
dkrnl <dkrnl@yandex.ru>
2017-06-07 11:41:38 +0700

    bootstrap: remove unused

--- app/_bootstrap.js
+++ app/_bootstrap.js
@@ -4,15 +4,15 @@
  * Licensed under MIT (https:/github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-import "bootstrap/transition.js";
-import "bootstrap/alert.js";
-import "bootstrap/button.js";
-import "bootstrap/carousel.js";
-import "bootstrap/collapse.js";
-import "bootstrap/dropdown.js";
-import "bootstrap/modal.js";
-import "bootstrap/tab.js";
-import "bootstrap/affix.js";
-import "bootstrap/scrollspy.js";
-import "bootstrap/tooltip.js";
-import "bootstrap/popover.js";
+//require("bootstrap/transition.js");
+//require("bootstrap/alert.js");
+//require("bootstrap/button.js");
+//require("bootstrap/carousel.js");
+//require("bootstrap/collapse.js");
+//require("bootstrap/dropdown.js");
+//require("bootstrap/modal.js");
+//require("bootstrap/tab.js");
+//require("bootstrap/affix.js");
+//require("bootstrap/scrollspy.js");
+//require("bootstrap/tooltip.js");
+//require("bootstrap/popover.js");
