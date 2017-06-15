40aa4d4db4788b5ae383fb84195b777d4d445c9f
Dmitri Pyatkov <dkrnl@yandex.ru>
2017-06-15 21:27:10 +0700

    ignored: /node_modules/

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,10 @@ const resourceUrl = (prefix) => {
 
 module.exports = {
 
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+
     devServer: {
         overlay: true,
         compress: false,
