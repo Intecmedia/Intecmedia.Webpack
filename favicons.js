6c6bbc1614c8376221006396bfc13ed1a9195b0f
dkrnl <dkrnl@yandex.ru>
2017-09-21 14:00:41 +0700

    fix icon validation

--- favicons.js
+++ favicons.js
@@ -22,10 +22,9 @@ module.exports.FavIcon = function FavIcon(options) {
     const mergedOptions = Object.assign({}, DEFAULT_FAVICON, options);
     const logoSize = ImageSize(mergedOptions.logo);
     if (!(logoSize && logoSize.type == 'png')) {
-        throw new Error('FavIcon \'' + mergedOptions.logo + '\': the file is not a valid image');
-    }
-    if (!(logoSize.width == 32 && logoSize.height == 32)) {
-        throw new Error('FavIcon \'' + mergedOptions.logo + '\': image size is not than (32 x 32)');
+        throw new Error(`FavIcon '${ mergedOptions.logo}': the file is not a valid image`);
+    } else if (!(logoSize.width == 32 && logoSize.height == 32)) {
+        throw new Error(`FavIcon '${mergedOptions.logo}': image size is not than (32 x 32)`);
     }
     return new FaviconsWebpackPlugin(mergedOptions);
 };
@@ -51,10 +50,9 @@ module.exports.AppIcon = function AppIcon(options) {
     const mergedOptions = Object.assign({}, DEFAULT_APPICON, options);
     const logoSize = ImageSize(mergedOptions.logo);
     if (!(logoSize && logoSize.type == 'png')) {
-        throw new Error('AppIcon \'' + mergedOptions.logo + '\': the file is not a valid image');
-    }
-    if (!(logoSize.width == 512 && logoSize.height == 512)) {
-        throw new Error('AppIcon \'' + mergedOptions.logo + '\': image size is not than (512 x 512)');
+        throw new Error(`AppIcon '${ mergedOptions.logo}': the file is not a valid image`);
+    } else  if (!(logoSize.width == 512 && logoSize.height == 512)) {
+        throw new Error(`AppIcon '${mergedOptions.logo}': image size is not than (512 x 512)`);
     }
     return new FaviconsWebpackPlugin(mergedOptions);
 };
