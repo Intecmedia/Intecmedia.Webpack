8535439b7220c228feabde7243d187aae108f898
dkrnl <dkrnl@yandex.ru>
2017-12-28 17:26:20 +0700

    fix html pretty

--- plugin.pretty.js
+++ plugin.pretty.js
@@ -3,7 +3,6 @@ const deepAssign = require('deep-assign');
 
 const DEFAULT_OPTIONS = {
     ocd: false,
-    unformatted: [],
     indent_inner_html: true,
     indent_char: ' ',
     indent_size: 4,
