79471dc603b571bb7f7b7bf2c2c24f004e9a73d7
Dmitri Pyatkov <dkrnl@yandex.ru>
2018-01-02 22:38:59 +0700

    comment unused code

--- source/js/app.js
+++ source/js/app.js
@@ -3,7 +3,7 @@ require('../css/app.scss');
 
 require('modernizr');
 require('picturefill');
-require('bootstrap');
+require('./bootstrap.js');
 
 jQuery(($) => {
     console.log(`NODE_ENV=${NODE_ENV}; DEBUG=${DEBUG}; jQuery=${$.fn.jquery};`);
