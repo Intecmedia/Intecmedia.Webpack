a58f7fec1446598bc802244303ff046e48eeeb27
dkrnl <dkrnl@yandex.ru>
2018-01-16 21:26:43 +0700

    .

--- imagemin.config.js
+++ imagemin.config.js
@@ -2,4 +2,13 @@ const svgoConfig = require('./svgo.config.js');
 
 module.exports = {
     svgo: svgoConfig,
+    optipng: {
+        optimizationLevel: 3,
+    },
+    gifsicle: {
+        optimizationLevel: 1,
+    },
+    jpegtran: {
+        progressive: false,
+    },
 };
