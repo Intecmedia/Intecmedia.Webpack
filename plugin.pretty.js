e8a55ebd4080c8debf92540ed2b0b0a281bc60d0
dkrnl <dkrnl@yandex.ru>
2018-01-18 16:07:17 +0700

    .

--- plugin.pretty.js
+++ plugin.pretty.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const pretty = require('pretty');
 const deepAssign = require('deep-assign');
 const validateOptions = require('schema-utils');
@@ -5,12 +6,17 @@ const weblog = require('webpack-log');
 
 const logger = weblog({ name: 'plugin-pretty' });
 
+const { 'tag-self-close': tagSelfClose } = JSON.parse(fs.readFileSync('./.htmllintrc', 'utf8'));
+
 const DEFAULT_OPTIONS = {
     ocd: false,
-    unformatted: ['code', 'pre', 'em', 'strong', 'span'],
+    unformatted: ['code', 'pre', 'em', 'strong', 'span'].concat(tagSelfClose),
     indent_inner_html: false,
     indent_char: ' ',
     indent_size: 4,
+    wrap_line_length: Number.MAX_SAFE_INTEGER,
+    preserve_newlines: true,
+    max_preserve_newlines: 1,
     sep: '\n',
 };
 
@@ -23,10 +29,12 @@ const OPTIONS_SCHEMA = {
         indent_inner_html: { type: 'boolean' },
         indent_char: { type: 'string' },
         indent_size: { type: 'integer' },
+        wrap_line_length: { type: 'integer' },
+        preserve_newlines: { type: 'boolean' },
+        max_preserve_newlines: { type: 'integer' },
         sep: { type: 'string' },
     },
 };
-
 
 module.exports = class PrettyPlugin {
     constructor(options) {
