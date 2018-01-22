cc675e7c3c9b2ac036bd1a8318da646987e68db7
dkrnl <dkrnl@yandex.ru>
2018-01-22 13:16:09 +0700

    breakpoints sizes

--- loader.resize.js
+++ loader.resize.js
@@ -59,3 +59,22 @@ module.exports = function ResizeLoader(content) {
 };
 
 module.exports.raw = true;
+
+const DEFAULT_SIZES = {
+    xs: 576, sm: 768, md: 992, lg: 1200, xl: 1900,
+};
+const DEFAULT_BREAKPOINTS = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+module.exports.breakpointsSizes = (breakpoints, sizes) => {
+    const sorted = DEFAULT_BREAKPOINTS.filter(i => breakpoints.includes(i));
+    const merged = Object.assign({}, sizes, DEFAULT_SIZES);
+
+    const result = {};
+    sorted.forEach((breakpoint, index) => {
+        result[breakpoint] = [
+            ...(sorted[index - 1] ? [`(min-width: ${merged[sorted[index - 1]]}px)`] : []),
+            ...(index !== sorted.length - 1 ? [`(max-width: ${merged[breakpoint] - 1}px)`] : []),
+        ].join(' and ');
+    });
+    return result;
+};
