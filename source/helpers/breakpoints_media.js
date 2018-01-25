e05f22c7acf0964e3521d94a24c82c247de3c2b9
dkrnl <dkrnl@yandex.ru>
2018-01-25 14:51:56 +0700

    .

--- source/helpers/breakpoints_media.js
+++ source/helpers/breakpoints_media.js
@@ -7,15 +7,10 @@ const DEFAULT_BREAKPOINTS = ['xs', 'sm', 'md', 'lg', 'xl'];
 module.exports = (breakpoints, sizes) => {
     const sorted = DEFAULT_BREAKPOINTS.filter(i => breakpoints.includes(i));
     const merged = Object.assign({}, sizes, DEFAULT_SIZES);
-
-    const result = new Map();
-    sorted.forEach((breakpoint, index) => {
-        result[breakpoint] = [
-            // not first
-            ...(index >= 1 && sorted[index - 1] ? [`(min-width: ${merged[sorted[index - 1]]}px)`] : []),
-            // not last
-            ...(index !== sorted.length - 1 ? [`(max-width: ${merged[breakpoint] - 1}px)`] : []),
-        ].join(' and ');
-    });
-    return result;
+    return sorted.map((breakpoint, index) => [breakpoint, [
+        // not first
+        ...(index >= 1 && sorted[index - 1] ? [`(min-width: ${merged[sorted[index - 1]]}px)`] : []),
+        // not last
+        ...(index !== sorted.length - 1 ? [`(max-width: ${merged[breakpoint] - 1}px)`] : []),
+    ].join(' and ')]);
 };
