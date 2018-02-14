41f2bc2bed5fe3c226ae6730962d9c92c6bbdb3e
dkrnl <dkrnl@yandex.ru>
2018-02-14 12:59:31 +0700

    .

--- source/js/polyfills.js
+++ source/js/polyfills.js
@@ -1,13 +1,13 @@
-/* global NODE_ENV */
+/* global NODE_ENV DEBUG */
 
-if (NODE_ENV === 'production') {
+if (NODE_ENV === 'production' || DEBUG) {
     // eslint-disable-next-line global-require
     require('picturefill');
     // eslint-disable-next-line global-require
     require('babel-polyfill');
 }
 
-if (NODE_ENV === 'production' && !Object.assign) {
+if ((NODE_ENV === 'production' || DEBUG) && !Object.assign) {
     // eslint-disable-next-line global-require
     Object.assign = require('object-assign');
 }
