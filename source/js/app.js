171d0301a3feef697b1cd25f337f0166276c72fe
dkrnl <dkrnl@yandex.ru>
2018-09-06 09:32:18 +0700

    add ready-js class

--- source/js/app.js
+++ source/js/app.js
@@ -4,6 +4,7 @@ require('./polyfills.js');
 require('./bootstrap.js');
 
 jQuery(($) => {
+    $(document.documentElement).addClass('ready-js');
     console.log(`NODE_ENV=${NODE_ENV}; DEBUG=${DEBUG}; jQuery=${$.fn.jquery};`);
     // Your code here
 });
