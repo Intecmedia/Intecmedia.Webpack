ad8fac803eed20519dde6646a232637bd9125731
dkrnl <dkrnl@yandex.ru>
2019-03-05 10:53:40 +0700

    add IntersectionObserver polyfills

--- source/js/polyfills.js
+++ source/js/polyfills.js
@@ -2,14 +2,17 @@
 
 if (NODE_ENV === 'production' || DEBUG) {
     // eslint-disable-next-line global-require
-    require('./polyfills/focus-within.js');
+    require('~/polyfills/focus-within.js');
 
     // eslint-disable-next-line global-require
-    require('./polyfills/object-fit-images.js');
+    require('~/polyfills/object-fit-images.js');
 
     // eslint-disable-next-line global-require
-    require('./polyfills/picturefill.js');
+    require('~/polyfills/picturefill.js');
 
     // eslint-disable-next-line global-require
-    require('./polyfills/object-assign');
+    require('~/polyfills/object-assign.js');
+
+    // eslint-disable-next-line global-require
+    require('~/polyfills/intersection-observer.js');
 }
---
+++ source/js/polyfills/intersection-observer.js
@@ -0,0 +1,5 @@
+if (!window.IntersectionObserver) {
+    // eslint-disable-next-line global-require
+    require('intersection-observer');
+}
+
