9a18c3ad0446c960848cad126f1725df442fc8a8
dkrnl <dkrnl@yandex.ru>
2019-03-12 12:54:23 +0700

    .

--- babel.config.js
+++ babel.config.js
@@ -2,22 +2,20 @@ const path = require('path');
 const ENV = require('./app.env.js');
 
 module.exports = {
-    options: {
-        plugins: [
-            '@babel/transform-runtime',
-        ],
-        presets: [
-            ['@babel/preset-env', {
-                modules: 'commonjs',
-                useBuiltIns: 'entry',
-                targets: { browsers: ENV.BROWSERS },
-            }],
-            ['airbnb', {
-                modules: true,
-                targets: { browsers: ENV.BROWSERS },
-            }],
-        ],
-    },
+    plugins: [
+        '@babel/transform-runtime',
+    ],
+    presets: [
+        ['@babel/preset-env', {
+            modules: 'commonjs',
+            useBuiltIns: 'entry',
+            targets: { browsers: ENV.BROWSERS },
+        }],
+        ['airbnb', {
+            modules: true,
+            targets: { browsers: ENV.BROWSERS },
+        }],
+    ],
     exclude: [
         // disable babel transform
         path.join(__dirname, 'node_modules'),
--- webpack.config.js
+++ webpack.config.js
@@ -421,7 +421,7 @@ module.exports = {
                         loader: 'babel-loader',
                         options: {
                             envName: ENV.NODE_ENV,
-                            ...BabelConfig.options,
+                            ...BabelConfig,
                         },
                     },
                 ],
