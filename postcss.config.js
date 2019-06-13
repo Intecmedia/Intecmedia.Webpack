b3621e402c08ef4fe94f63795c001b0f46e722d3
dkrnl <dkrnl@yandex.ru>
2019-06-13 17:57:03 +0700

    add postcss-webpcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 /* eslint global-require: "off" */
-const path = require('path');
 const ENV = require('./app.env.js');
-const { URLSearchParams } = require('url');
 
 module.exports = {
     plugins: [
@@ -20,20 +18,7 @@ module.exports = {
             require('postcss-object-fit-images')(),
             require('postcss-flexbugs-fixes')(),
             require('postcss-will-change')(),
-            require('webpcss').default({
-                webpClass: '.webp',
-                noWebpClass: '',
-                replace_from: /.(png|jpg|jpeg)(\?.*)?$/i,
-                replace_to: ({ url }) => {
-                    const [urlPath, searchPath = ''] = url.split('?', 2);
-                    const searchParams = new URLSearchParams(searchPath);
-                    const name = path.basename(urlPath, path.extname(urlPath));
-                    searchParams.set('resize', '');
-                    searchParams.set('format', 'webp');
-                    searchParams.set('name', name);
-                    return [urlPath, searchParams].join('?');
-                },
-            }),
+            require('./postcss.webp.js')(),
             require('autoprefixer')({ overrideBrowserslist: ENV.BROWSERS }), // this always last
             require('cssnano')({
                 preset: ['default', {
---
+++ postcss.webp.js
@@ -0,0 +1,19 @@
+/* eslint global-require: "off" */
+const path = require('path');
+const webpcss = require('webpcss').default;
+const { URLSearchParams } = require('url');
+
+module.exports = () => webpcss({
+    webpClass: '.webp',
+    noWebpClass: '',
+    replace_from: /.(png|jpg|jpeg)(\?.*)?$/i,
+    replace_to: ({ url }) => {
+        const [urlPath, searchPath = ''] = url.split('?', 2);
+        const searchParams = new URLSearchParams(searchPath);
+        const name = path.basename(urlPath, path.extname(urlPath));
+        searchParams.set('resize', '');
+        searchParams.set('format', 'webp');
+        searchParams.set('name', name);
+        return [urlPath, searchParams].join('?');
+    },
+});
