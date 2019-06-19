2602152a5a8c71e73a77e2215e88fcef5056546c
dkrnl <dkrnl@yandex.ru>
2019-06-19 15:09:59 +0700

    add w3c validator

--- script.html-lint.js
+++ script.html-lint.js
@@ -1,44 +1,67 @@
+/* eslint max-len: "off" */
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
+const slash = require('slash');
 const htmllint = require('htmllint');
 const weblog = require('webpack-log');
+const { fileURLToPath } = require('url');
+const W3CValidator = require('node-w3c-validator');
 
-const ENV = require('./app.env.js');
-
-const logger = weblog({ name: 'html-lint' });
+const loggerHtmlLint = weblog({ name: 'html-lint' });
+const loggerW3CValidator = weblog({ name: 'w3c-validator' });
 const htmllintrc = JSON.parse(fs.readFileSync('./.htmllintrc', 'utf8'));
 
 if (htmllintrc.plugins) {
     htmllint.use(htmllintrc.plugins);
 }
 
-glob(`${ENV.OUTPUT_PATH}/**/*.html`, {
-    ignore: [
-        `${ENV.OUTPUT_PATH}/google*.html`,
-        `${ENV.OUTPUT_PATH}/yandex_*.html`,
-    ],
+const callbackHtmlLint = () => glob('./build/**/*.html', {
+    ignore: [],
 }, (error, files) => {
     if (error) throw error;
 
     files.forEach((filename) => {
-        const relative = path.relative(__dirname, filename);
+        const relative = slash(path.relative(__dirname, filename));
         const html = fs.readFileSync(filename, 'utf8').toString();
         htmllint(html, htmllintrc).then((issues) => {
             if (issues === false) {
-                logger.info(`skipped ${relative}`);
+                loggerHtmlLint.info(`skipped ${relative}`);
                 return;
             }
             issues.forEach((issue) => {
-                logger.info(`${relative}: line ${issue.line} col ${issue.column}`);
-                logger.warn(`${htmllint.messages.renderIssue(issue)}\n`);
+                loggerHtmlLint.info(`${relative}: line ${issue.line} col ${issue.column}`);
+                loggerHtmlLint.warn(`${htmllint.messages.renderIssue(issue)}\n`);
             });
-            logger.error(`found ${issues.length} errors out of ${files.length} files`);
+            loggerHtmlLint.error(`found ${issues.length} errors out of ${files.length} files`);
             if (issues.length > 0) {
-                process.exit(1);
+                process.exitCode = 1;
             }
         }).catch((exception) => {
             throw new Error(`error in ${relative}: ${exception}`);
         });
     });
 });
+
+const callbackW3CValidator = callback => W3CValidator('./build/**/*.html', {
+    format: 'json',
+    skipNonHtml: true,
+    verbose: false,
+    stream: false,
+}, (exception, output) => {
+    if (exception === null) {
+        return;
+    }
+    const { messages } = JSON.parse(output);
+    if (messages.length > 0) {
+        process.exitCode = 1;
+    }
+    messages.forEach((message) => {
+        const relative = slash(path.relative(__dirname, fileURLToPath(message.url)));
+        loggerW3CValidator.info(`${relative}: line [${message.lastLine}] col [${message.firstColumn}-${message.lastColumn}]`);
+        loggerW3CValidator.warn(`${message.message}\n`);
+    });
+    callback();
+});
+
+callbackW3CValidator(callbackHtmlLint);
