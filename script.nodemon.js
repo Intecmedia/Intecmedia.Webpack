a74a78103ad008527e43e7fd34ddbddb0b1b309f
dkrnl <dkrnl@yandex.ru>
2019-06-19 17:39:40 +0700

    nodemon: add logger

--- script.nodemon.js
+++ script.nodemon.js
@@ -1,16 +1,17 @@
 const nodemon = require('nodemon');
 const nodemonCli = require('nodemon/lib/cli');
 const deepMerge = require('lodash.merge');
+const weblog = require('webpack-log');
 
 const ENV = require('./app.env.js');
+const logger = weblog({ name: 'nodemon' });
 
 const nodemonConfig = {
     delay: '2500',
     ext: 'js,html',
     ignore: [
-        '.hg', '.git', '.svn',
-        'build', 'node_modules',
-        'source/js', 'source/partials', 'source/html.data.js',
+        '**/.hg/**', '**/.git/**', '**/.svn/**',
+        'build', 'source/js', 'source/partials', 'source/html.data.js',
     ].concat(ENV.SITEMAP.map(i => i.template)),
     verbose: true,
 };
@@ -18,3 +19,12 @@ const nodemonConfig = {
 const options = deepMerge({}, nodemonConfig, nodemonCli.parse(process.argv));
 
 nodemon(options);
+
+nodemon.on('start', () => {
+    logger.info('App has started');
+}).on('quit', () => {
+    logger.warn('App has quit');
+    process.exit();
+}).on('restart', (files) => {
+    logger.info('App restarted due to: ', files);
+});
