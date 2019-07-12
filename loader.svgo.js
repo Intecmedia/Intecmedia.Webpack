795549c9286428e940a166ad7cf69b5de71ca718
dkrnl <dkrnl@yandex.ru>
2019-07-12 16:43:45 +0700

    add svgo logger

--- loader.svgo.js
+++ loader.svgo.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const slash = require('slash');
 const SVGO = require('svgo');
+const weblog = require('webpack-log');
 const { SvgoIdPrefix, SvgoPrefixConfig } = require('./svgo.config.js');
+
+const logger = weblog({ name: 'loader-svgo' });
 
 module.exports = function SvgLoader(content) {
     const loaderContext = this;
@@ -11,7 +15,12 @@ module.exports = function SvgLoader(content) {
     const options = SvgoPrefixConfig(new SvgoIdPrefix(`svgo-${name.toLowerCase()}-`));
     const svgoInstance = new SVGO(options);
 
+    const relativePath = slash(path.relative(__dirname, loaderContext.resourcePath));
+    logger.info(`load: '${relativePath}'...`);
+
     svgoInstance.optimize(content).then((result) => {
+        logger.info(`done: ${JSON.stringify(result.info)}'`);
+
         const exportString = `module.exports = ${JSON.stringify(result.data)}`;
         loaderCallback(null, exportString);
     }).catch((error) => {
