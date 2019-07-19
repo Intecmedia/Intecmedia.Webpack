985d1ec78290750bf993ace94fe7e81c446b259d
dkrnl <dkrnl@yandex.ru>
2019-07-19 10:41:09 +0700

    add .htmlvalidatorignore config

--- script.html-validator.js
+++ script.html-validator.js
@@ -10,6 +10,10 @@ const logger = weblog({ name: 'html-validator' });
 
 const ENV = require('./app.env.js');
 
+const ignoreLines = fs.readFileSync('./.htmlvalidatorignore')
+    .toString().trim().split('\n')
+    .map(i => i.toLowerCase().trim());
+
 const errorsLogger = {
     error: logger.error,
     'non-document-error': logger.error,
@@ -26,19 +30,29 @@ glob(`${ENV.OUTPUT_PATH}/**/*.html`, {
         const relative = slash(path.relative(__dirname, filename));
         const html = fs.readFileSync(filename, 'utf8').toString();
         const result = await validator({ format: 'json', data: html });
-        if (!result.messages || !result.messages.length) {
-            logger.info(`skipped ${relative}`);
-            return;
+
+        let skipped = false;
+        if (result.messages && result.messages.length) {
+            result.messages.forEach((message) => {
+                if (message.message && ignoreLines.includes(message.message.toLowerCase().trim())) {
+                    skipped = true;
+                    return;
+                }
+                if (message.type === 'error') {
+                    process.exitCode = 1;
+                }
+                const log = errorsLogger[message.type] || logger.error;
+                log(`${relative}: line ${message.lastLine || 0} col [${message.firstColumn || 0}-${message.lastColumn || 0}]`);
+                log(`${message.type}: ${JSON.stringify(message.message)}.`);
+                log(`${JSON.stringify(message.extract)}.`);
+                console.log('');
+            });
+        } else {
+            skipped = true;
         }
-        result.messages.forEach((message) => {
-            if (message.type === 'error') {
-                process.exitCode = 1;
-            }
-            const log = errorsLogger[message.type] || logger.error;
-            log(`${relative}: line ${message.lastLine || 0} col [${message.firstColumn || 0}-${message.lastColumn || 0}]`);
-            log(`${message.type}: ${JSON.stringify(message.message)}.`);
-            log(`${JSON.stringify(message.extract)}.`);
-            console.log('');
-        });
+
+        if (skipped) {
+            logger.info(`skipped ${relative}`);
+        }
     });
 });
