246acfe07d44d4c0946e72fcc4f29a9a11d5ae93
dkrnl <dkrnl@yandex.ru>
2019-09-11 09:49:27 +0700

    add fluid component

---
+++ source/js/components/fluid.js
@@ -0,0 +1,18 @@
+export default function fluidValue(value, minValue, maxValue, minRange, maxRange) {
+    let result = minValue;
+    if (value <= minRange) {
+        result = minValue + (((maxValue - minValue) * (value - minRange)) / (maxRange - minRange));
+    }
+    if (value >= maxRange) {
+        result = maxValue;
+    }
+    return result;
+}
+
+export function fluidWidth(minValue, maxValue, minRange = 320, maxRange = 1200) {
+    return fluidValue(window.innerWidth, minValue, maxValue, minRange, maxRange);
+}
+
+export function fluidHeight(minValue, maxValue, minRange = 320, maxRange = 480) {
+    return fluidValue(window.innerHeight, minValue, maxValue, minRange, maxRange);
+}
