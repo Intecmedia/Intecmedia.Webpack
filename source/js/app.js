37c861821cea32d3d2fac15bd50acd2bcdbd3702
dkrnl <dkrnl@yandex.ru>
2019-10-02 14:23:32 +0700

    eslint: add eslint-plugin-compat

--- source/js/app.js
+++ source/js/app.js
@@ -7,20 +7,5 @@ require('~/components/bootstrap.js');
 jQuery(($) => {
     $(document.documentElement).addClass('ready-js');
     console.log(`NODE_ENV=${NODE_ENV}; DEBUG=${DEBUG}; jQuery=${$.fn.jquery};`);
-
-    const ro = new ResizeObserver((entries) => {
-        for (const entry of entries) {
-            const {
-                left, top, width, height,
-            } = entry.contentRect;
-
-            console.log('Element:', entry.target);
-            console.log(`Element's size: ${width}px x ${height}px`);
-            console.log(`Element's paddings: ${top}px ; ${left}px`);
-        }
-    });
-
-    ro.observe(document.body);
-
     // Your code here
 });
