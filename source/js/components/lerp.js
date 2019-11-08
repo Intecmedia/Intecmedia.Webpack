594b47bcc5eed448cabb0bb338b5415313248618
dkrnl <dkrnl@yandex.ru>
2019-11-08 13:51:39 +0700

    add smoothstep

--- source/js/components/lerp.js
+++ source/js/components/lerp.js
@@ -1,3 +1,4 @@
+// bare-bones linear interpolation function
 export default function lerp(start, end, factor) {
     return start * (1 - factor) + end * factor;
 }
---
+++ source/js/components/smoothstep.js
@@ -0,0 +1,5 @@
+// Bare-bones smoothstep function (cubic Hermite interpolation), returning a value in the range 0.0 to 1.0.
+export default function smoothstep(min, max, value) {
+    const x = Math.max(0, Math.min(1, (value - min) / (max - min)));
+    return x * x * (3 - 2 * x);
+}
