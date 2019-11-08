1c28ceb8d188df95d19a69a5a7870618deb0fc9a
dkrnl <dkrnl@yandex.ru>
2019-11-08 13:57:09 +0700

    add inverse linear interpolate

--- source/js/components/lerp.js
+++ source/js/components/lerp.js
@@ -1,4 +1,11 @@
-// bare-bones linear interpolation function
+import clamp from './clamp';
+
+// linear interpolation function
 export default function lerp(start, end, factor) {
     return start * (1 - factor) + end * factor;
+}
+
+// inverse linear interpolation function
+export function invlerp(a, b, v) {
+    return clamp((v - a) / (b - a));
 }
