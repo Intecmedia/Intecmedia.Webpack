31e2d987fd2467d41ebe31820c1dd68c7754abb9
dkrnl <dkrnl@yandex.ru>
2019-11-13 11:35:41 +0700

    add @babel/plugin-proposal-decorators

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,8 @@ module.exports = {
             '@babel/transform-runtime',
             ['@babel/plugin-proposal-optional-chaining', { loose: false }],
             ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
         ],
         presets: [
             ['@babel/preset-env', {
