aefae8317a9deea8864e1b41bd8c167a7af4eb96
dkrnl <dkrnl@yandex.ru>
2019-11-19 14:08:49 +0700

    .

--- postcss.resolve-absolute.js
+++ postcss.resolve-absolute.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 /* eslint global-require: "off", "compat/compat": "off" */
 
+const ABSOLUTE_PATTERN = /^\/([^/])/;
+const ABSOLUTE_REPLACE = '../$1';
+
 module.exports = () => require('postcss-url')({
-    url: (asset) => asset.url.replace(/^\/([^/])/, '../$1'), // resolve absolute urls
+    url: (asset) => asset.url.replace(ABSOLUTE_PATTERN, ABSOLUTE_REPLACE), // resolve absolute urls
 });
