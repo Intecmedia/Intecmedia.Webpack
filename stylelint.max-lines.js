2696d11749bb65a75e3c5265b610e64659d18a8c
dkrnl <dkrnl@yandex.ru>
2019-11-22 11:32:03 +0700

    stylelint: add max-lines rule

---
+++ stylelint.max-lines.js
@@ -0,0 +1,33 @@
+/* eslint-env node */
+/* eslint "compat/compat": "off" */
+
+const stylelint = require('stylelint');
+
+const ruleName = 'intecmedia/max-lines';
+const messages = stylelint.utils.ruleMessages(ruleName, {
+    expected: (maxLines, countLines) => [
+        `Expected no more than ${maxLines} lines.`,
+        `Found ${countLines} lines.`,
+    ].join(' '),
+});
+
+module.exports = stylelint.createPlugin(ruleName, (maxLines) => ((postcssRoot, postcssResult) => {
+    const validOptions = stylelint.utils.validateOptions(postcssResult, ruleName, {
+        actual: maxLines,
+        possible: Number.isInteger,
+    });
+    if (!validOptions) return undefined;
+
+    const countLines = postcssRoot.source.input.css.split(/\r\n|\n/).length;
+    if (countLines <= maxLines) return undefined;
+
+    return stylelint.utils.report({
+        message: messages.expected(maxLines, countLines),
+        node: postcssRoot,
+        result: postcssResult,
+        ruleName,
+    });
+}));
+
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
