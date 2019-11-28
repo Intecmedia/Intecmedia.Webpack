df49e963b12fce8a6c37a163b3a4390ebd23c54c
dkrnl <dkrnl@yandex.ru>
2019-11-28 14:44:11 +0700

    .

---
+++ source/js/utils/random.js
@@ -0,0 +1,14 @@
+// Random float from <low, high> interval
+export default function randFloat(low, high) {
+    return low + Math.random() * (high - low);
+}
+
+// Random integer from <low, high> interval
+export function randInt(low, high) {
+    return low + Math.floor(Math.random() * (high - low + 1));
+}
+
+// Random float from <-range/2, range/2> interval
+export function randFloatSpread(range) {
+    return range * (0.5 - Math.random());
+}
