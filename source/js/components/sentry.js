bcc49ca2301743e73fcd138ff43e5bf726d8e44b
dkrnl <dkrnl@yandex.ru>
2019-12-03 10:53:33 +0700

    .

--- source/js/components/sentry.js
+++ source/js/components/sentry.js
@@ -3,18 +3,27 @@ if ((NODE_ENV === 'production' || DEBUG) && APP.SENTRY.dsn) {
     // eslint-disable-next-line global-require
     const { init: sentryInit } = require('@sentry/browser');
 
+    const sentryCheckIgnore = (event) => {
+        // Detect if we got a ReportingObserver event
+        if (event?.message?.indexOf('ReportingObserver') === 0) {
+            // And check whether sourceFile points to chrome-extension.
+            if (event?.extra?.body?.sourceFile?.indexOf('chrome-extension') === 0) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     sentryInit({
         debug: DEBUG || false,
         dsn: APP.SENTRY.dsn,
         beforeSend(event) {
-            // Detect if we got a ReportingObserver event
-            if (event?.message?.indexOf('ReportingObserver') === 0) {
-                // And check whether sourceFile points to chrome-extension.
-                if (event?.extra?.body?.sourceFile?.indexOf('chrome-extension') === 0) {
-                    return null;
+            if (sentryCheckIgnore(event)) {
+                if (NODE_ENV === 'development' || DEBUG) {
+                    console.log('[sentry]', event);
                 }
+                return null;
             }
-            // Otherwise, just let it through
             return event;
         },
         ignoreErrors: APP.SENTRY.ignoreErrors || [],
