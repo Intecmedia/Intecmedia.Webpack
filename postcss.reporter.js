3110b6362664a65957d41ece131e705b8e449c02
dkrnl <dkrnl@yandex.ru>
2019-12-12 17:03:33 +0700

    .

--- postcss.reporter.js
+++ postcss.reporter.js
@@ -8,6 +8,7 @@ const reporterFormatter = require('postcss-reporter/lib/formatter.js')({});
 module.exports = () => require('postcss-reporter')({
     formatter: (input) => {
         input.messages.forEach((message) => {
+            if (!(message.node && message.node.source && message.node.source.input)) return;
             const { css } = message.node.source.input;
             const index = lineColumn(css).toIndex({ line: message.line, col: message.column });
             const ellipsis = css.substring(index - lineEllipsis, index + lineEllipsis);
