6bc7228375710c4d0024299150cef67768a64058
dkrnl <dkrnl@yandex.ru>
2019-12-16 13:23:06 +0700

    .

--- source/js/components/svg-srite.js
+++ source/js/components/svg-srite.js
@@ -1,8 +1,5 @@
 const svg4everybody = require('svg4everybody');
 
-function requireAll(r) {
-    r.keys().forEach(r);
-}
-console.log(require.context('../../img/svg-sprite/', true, /\.svg$/));
+require.context('../../img/svg-sprite/', true, /\.svg$/);
 
 svg4everybody();
