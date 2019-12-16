e974cd3bdc17e4c501143751ca7351762f8f8b87
dkrnl <dkrnl@yandex.ru>
2019-12-16 14:04:17 +0700

    .

--- source/js/components/svg-srite.js
+++ source/js/components/svg-srite.js
@@ -2,4 +2,13 @@ const svg4everybody = require('svg4everybody');
 
 require.context('../../img/svg-sprite/', true, /\.svg$/);
 
-svg4everybody();
+jQuery(($) => {
+    svg4everybody();
+
+    $(window).on('pushState replaceState', () => {
+        // Barba events
+        setTimeout(() => {
+            svg4everybody();
+        }, 0);
+    });
+});
