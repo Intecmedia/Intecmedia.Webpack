66ee1c0dfe4934d816d259d3ca95949672bf380e
dkrnl <dkrnl@yandex.ru>
2019-12-17 11:58:29 +0700

    .

--- plugin.svg-sprite.js
+++ plugin.svg-sprite.js
@@ -2,6 +2,8 @@
 
 const SpriteLoaderPluginOriginal = require('svg-sprite-loader/plugin');
 
+const APP = require('./app.config.js');
+
 const NODE_PLACEHOLDER = '%__SVG_SPRITE_NODE__%';
 
 module.exports = class SpriteLoaderPlugin extends SpriteLoaderPluginOriginal {
@@ -13,9 +15,9 @@ module.exports = class SpriteLoaderPlugin extends SpriteLoaderPluginOriginal {
                 (htmlPluginData, callback) => {
                     const { sprites = {} } = htmlPluginData.assets;
                     const inner = Object.keys(sprites).map((filename) => [
-                        `<!-- ${filename} -->`,
+                        `<!-- ${JSON.stringify(APP.PUBLIC_PATH + filename)} -->`,
                         sprites[filename],
-                        `<!-- /${filename} -->`,
+                        `<!-- /${JSON.stringify(APP.PUBLIC_PATH + filename)} -->`,
                     ].join('\n')).join('\n');
                     const html = htmlPluginData.html.replace(NODE_PLACEHOLDER, inner);
                     callback(null, Object.assign(htmlPluginData, { html }));
