04adef8218f4af1fdbd673bd9ee5736a259ab7da
dkrnl <dkrnl@yandex.ru>
2019-12-17 11:42:21 +0700

    .

--- plugin.svg-sprite.js
+++ plugin.svg-sprite.js
@@ -11,8 +11,13 @@ module.exports = class SpriteLoaderPlugin extends SpriteLoaderPluginOriginal {
             compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
                 'SpriteLoaderPlugin',
                 (htmlPluginData, callback) => {
-                    const { sprites } = htmlPluginData.assets;
-                    const html = htmlPluginData.html.replace(NODE_PLACEHOLDER, Object.values(sprites).join('\n'));
+                    const { sprites = {} } = htmlPluginData.assets;
+                    const inner = Object.keys(sprites).map((filename) => [
+                        `<!-- ${filename} -->`,
+                        sprites[filename],
+                        `<!-- /${filename} -->`,
+                    ].join('\n')).join('\n');
+                    const html = htmlPluginData.html.replace(NODE_PLACEHOLDER, inner);
                     callback(null, Object.assign(htmlPluginData, { html }));
                 },
             );
