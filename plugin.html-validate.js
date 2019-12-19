2c140c24fc7c7fd5cfda8c53522e7820347f8dc2
dkrnl <dkrnl@yandex.ru>
2019-12-19 17:14:14 +0700

    .

--- plugin.html-validate.js
+++ plugin.html-validate.js
@@ -9,25 +9,27 @@ class ImgPictureRequired extends Rule {
     }
 
     setup() {
-        this.on('dom:ready', (event) => {
-            const imgs = event.document.getElementsByTagName('img');
-            const ignores = this.options.ignore ? event.document.querySelectorAll(this.options.ignore) : [];
-            imgs.forEach((img) => {
-                if (ignores && ignores.some((i) => nodeEqual(i, img))) {
-                    return;
+        this.on('dom:ready', (event) => this.domReady(event));
+    }
+
+    domReady(event) {
+        const imgs = event.document.getElementsByTagName('img');
+        const ignores = this.options.ignore ? event.document.querySelectorAll(this.options.ignore) : [];
+        imgs.forEach((img) => {
+            if (ignores && ignores.some((i) => nodeEqual(i, img))) {
+                return;
+            }
+            const picture = img.parent;
+            if (!(picture && picture.nodeName.toLowerCase() === 'picture')) {
+                this.report(img, '<img> required <picture> element.');
+                return;
+            }
+            if (this.options.webp) {
+                const sources = picture.querySelectorAll('> source[type="image/webp"]');
+                if (!(sources && sources.length)) {
+                    this.report(picture, '<picture> required <source type="image/webp"> element.');
                 }
-                const picture = img.parent;
-                if (!(picture && picture.nodeName.toLowerCase() === 'picture')) {
-                    this.report(img, '<img> required <picture> element.');
-                    return;
-                }
-                if (this.options.webp) {
-                    const sources = picture.querySelectorAll('> source[type="image/webp"]');
-                    if (!(sources && sources.length)) {
-                        this.report(picture, '<picture> required <source type="image/webp"> element.');
-                    }
-                }
-            });
+            }
         });
     }
 }
