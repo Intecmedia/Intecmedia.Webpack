bee03487454a02aadd54015fcaf506d3a49254ff
dkrnl <dkrnl@yandex.ru>
2019-12-24 09:22:30 +0700

    svgo-sprite: add prefix id`s

--- webpack.config.js
+++ webpack.config.js
@@ -450,7 +450,8 @@ module.exports = {
                     symbolId: (filePath) => {
                         const spriteDir = path.join(ENV.SOURCE_PATH, 'img/svg-sprite');
                         const relativePath = slash(path.relative(spriteDir, path.normalize(filePath)));
-                        return path.basename(relativePath, '.svg').replace(path.posix.sep, '-');
+                        const symbolId = path.basename(relativePath, '.svg').replace(path.posix.sep, '-');
+                        return `svg-sprite-${symbolId}`;
                     },
                 },
             },
