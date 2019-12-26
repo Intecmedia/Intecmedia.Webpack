7bac1a2b5a570ba033afeccbf185890846057d6c
dkrnl <dkrnl@yandex.ru>
2019-12-26 14:07:07 +0700

    svgo: convertTransform=false

--- svgo.config.js
+++ svgo.config.js
@@ -19,6 +19,7 @@ const SvgoPrefixConfig = (prefix = false) => ({
         },
         { convertShapeToPath: false },
         { convertStyleToAttrs: false },
+        { convertTransform: false },
         { removeViewBox: false },
         { removeUselessDefs: false },
     ],
