f79dc0b3a99cc3a73247ba8ef62097c46ddb4c34
dkrnl <dkrnl@yandex.ru>
2020-01-30 10:43:56 +0700

    .

--- postcss.stylelint.js
+++ postcss.stylelint.js
@@ -1,15 +1,15 @@
 /* eslint-env node */
 /* eslint global-require: "off", "compat/compat": "off" */
 
+const stylelintrc = require('./.stylelintrc.json');
+
 module.exports = () => require('stylelint')({
     config: {
         plugins: [
             'stylelint-csstree-validator',
         ],
         rules: {
-            'csstree/validator': {
-                ignore: ['font-display'],
-            },
+            'csstree/validator': stylelintrc.rules['csstree/validator'],
         },
     },
 });
