970b5814b136276cdddaf946a2a82339cc2760a2
dkrnl <dkrnl@yandex.ru>
2020-02-13 15:28:50 +0700

    .

--- source/js/polyfills/classlist.js
+++ source/js/polyfills/classlist.js
@@ -1,7 +1,7 @@
 /* eslint 'compat/compat': 'off' */
 
 // for polyfill use only require
-if (!Element.prototype.classList) {
+if (!('classList' in Element.prototype)) {
     // eslint-disable-next-line global-require
     require('classlist.js');
 }
