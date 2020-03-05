7b345ac809537a14adffb67622f38712ffe7aa6c
dkrnl <dkrnl@yandex.ru>
2020-03-05 14:42:29 +0700

    .

--- postcss.stylelint.js
+++ postcss.stylelint.js
@@ -1,20 +1,6 @@
 /* eslint-env node */
 /* eslint global-require: 'off', 'compat/compat': 'off' */
 
-module.exports = () => require('stylelint')({
-    config: {
-        plugins: [
-            'stylelint-csstree-validator',
-            'stylelint-use-nesting',
-        ],
-        rules: {
-            'no-duplicate-selectors': true,
-            'csstree/validator': {
-                ignore: ['font-display'],
-            },
-            'csstools/use-nesting': [true, {
-                severity: 'error',
-            }],
-        },
-    },
-});
+const stylelintrc = require('./.stylelintrc.postcss.json');
+
+module.exports = () => require('stylelint')(stylelintrc);
