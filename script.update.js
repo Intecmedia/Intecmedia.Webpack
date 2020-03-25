d2829b4ca502ab787386308ecd0690c59c9221e4
dkrnl <dkrnl@yandex.ru>
2020-03-25 18:12:07 +0700

    .

--- script.update.js
+++ script.update.js
@@ -2,19 +2,19 @@
 /* eslint "compat/compat": "off" */
 const childProcess = require('child_process');
 
-console.log('npm outdate\n');
+console.log('npm outdate');
 childProcess.exec('npm outdate --json', (err, stdout, stderr) => {
     if (stderr) throw stderr;
 
     const outdated = Object.entries(JSON.parse(stdout));
 
     const missing = outdated.filter(([, pkgVersion]) => pkgVersion.location === '');
-    console.log(`missing: ${missing.length} packages\n`);
+    console.log(`missing: ${missing.length} packages`);
 
     // install missing package
     missing.forEach(([pkgName, pkgVersion]) => {
-        const command = `npm install ${pkgName}@${pkgVersion.wanted}\n`;
-        childProcess.exec(command);
-        console.log(command);
+        const installCommand = `npm install ${pkgName}@${pkgVersion.wanted}`;
+        console.log(installCommand);
+        childProcess.execSync(installCommand, { stdio: 'inherit' });
     });
 });
