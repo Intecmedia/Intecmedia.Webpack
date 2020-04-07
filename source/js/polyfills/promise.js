5e7c2d9666ab6a8fa5747e01a1a2142666334de8
dkrnl <dkrnl@yandex.ru>
2020-04-07 18:06:14 +0700

    add Promise.finally polyfill

--- source/js/polyfills/promise.js
+++ source/js/polyfills/promise.js
@@ -5,3 +5,8 @@ if (!window.Promise) {
     // eslint-disable-next-line global-require
     require('core-js/stable/promise');
 }
+
+if (window.Promise && !window.Promise.finally) {
+    // eslint-disable-next-line global-require
+    require('core-js/stable/promise/finally');
+}
