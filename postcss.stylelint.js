b25dadadf84c1d995b57fc1bbdd710d0706411fe
dkrnl <dkrnl@yandex.ru>
2020-04-17 21:32:40 +0700

    .

--- postcss.stylelint.js
+++ postcss.stylelint.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 /* eslint global-require: 'off', 'compat/compat': 'off' */
 
+const ENV = require('./app.env.js');
 const stylelintrc = require('./.stylelintrc.postcss.json');
+
+stylelintrc.rules['plugin/no-unsupported-browser-features'][1].browsers = ENV.BROWSERS;
 
 module.exports = () => require('stylelint')(stylelintrc);
