4a916e8bb909db6fad2a62f5df4c1661fadbba8e
dkrnl <dkrnl@yandex.ru>
2020-06-26 14:08:52 +0700

    .

--- webpack.config.js
+++ webpack.config.js
@@ -234,7 +234,7 @@ module.exports = {
             inject: !path.basename(template).startsWith('_'),
             minify: (ENV.PROD || ENV.DEBUG ? ({
                 html5: true,
-                collapseWhitespace: APP.HTML_PRETTY,
+                collapseWhitespace: !APP.HTML_PRETTY,
                 removeComments: !APP.HTML_PRETTY,
                 decodeEntities: !APP.HTML_PRETTY,
                 minifyCSS: !APP.HTML_PRETTY,
