fa5fad713d4a97f85a81f718cf1f03004d0a2e37
dkrnl <dkrnl@yandex.ru>
2020-06-26 12:50:05 +0700

    update expose-loader

--- webpack.config.js
+++ webpack.config.js
@@ -311,13 +311,10 @@ module.exports = {
             // javascript loaders
             {
                 test: require.resolve('jquery'),
-                use: [{
-                    loader: 'expose-loader',
-                    options: 'jQuery',
-                }, {
-                    loader: 'expose-loader',
-                    options: '$',
-                }],
+                loader: 'expose-loader',
+                options: {
+                    exposes: ['$', 'jQuery'],
+                },
             },
             ...(ENV.USE_LINTERS ? [{
                 enforce: 'pre',
