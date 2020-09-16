60383273f5d12479722c734c6fa8ba53a9b35eb5
dkrnl <dkrnl@yandex.ru>
2020-09-16 15:58:56 +0700

    .

--- postcss.reporter.js
+++ postcss.reporter.js
@@ -5,15 +5,19 @@ const lineEllipsis = 80;
 const lineColumn = require('line-column');
 const reporterFormatter = require('postcss-reporter/lib/formatter.js')({});
 
-module.exports = () => require('postcss-reporter')({
-    formatter: (input) => {
-        input.messages.forEach((message) => {
-            if (!(message.node && message.node.source && message.node.source.input)) return;
-            const { css } = message.node.source.input;
-            const index = lineColumn(css).toIndex({ line: message.line, col: message.column });
-            const ellipsis = css.substring(index - lineEllipsis, index + lineEllipsis).trim();
-            message.text += `:\n…\n${ellipsis}\n…\n`;
-        });
-        return reporterFormatter(input);
-    },
-});
+module.exports = () => {
+    const plugin = require('postcss-reporter')({
+        formatter: (input) => {
+            input.messages.forEach((message) => {
+                if (!(message.node && message.node.source && message.node.source.input)) return;
+                const { css } = message.node.source.input;
+                const index = lineColumn(css).toIndex({ line: message.line, col: message.column });
+                const ellipsis = css.substring(index - lineEllipsis, index + lineEllipsis).trim();
+                message.text += `:\n…\n${ellipsis}\n…\n`;
+            });
+            return reporterFormatter(input);
+        },
+    });
+    plugin.postcssPlugin = 'postcss.reporter.js';
+    return plugin;
+};
