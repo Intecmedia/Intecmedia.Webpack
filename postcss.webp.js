16a84ea5637bd9d2538160ecfcd19bfa92972654
dkrnl <dkrnl@yandex.ru>
2020-09-16 13:51:15 +0700

    remove webpcss

--- postcss.webp.js
+++ postcss.webp.js
@@ -2,23 +2,40 @@
 /* eslint "compat/compat": "off" */
 
 const path = require('path');
-const { default: webpcss } = require('webpcss');
 const { URLSearchParams } = require('url');
+const postcssUrl = require('postcss-url');
 
 const IMAGE_PATTERN = /.(png|jpg|jpeg)(\?.*)?$/i;
 
-module.exports = () => webpcss({
-    noWebpClass: '',
-    replace_from: IMAGE_PATTERN,
-    webpClass: 'html.webp',
-    replace_to(input) {
-        if (!IMAGE_PATTERN.test(input.url)) return input.url;
-        const [request, search = ''] = input.url.split('?', 2);
+module.exports = () => postcssUrl({
+    url(asset, dir, options, decl) {
+        const { originUrl } = asset;
+
+        if (!IMAGE_PATTERN.test(originUrl)) return originUrl;
+
+        const [request, search = ''] = originUrl.split('?', 2);
         const params = new URLSearchParams(search);
-        if (params.has('resize')) return input.url;
+        if (params.has('resize')) return originUrl;
+
         params.set('resize', '');
         params.set('format', 'webp');
         params.set('name', path.basename(request, path.extname(request)));
-        return [request, params].join('?');
+        const webpUrl = [request, params].join('?');
+
+        const originRule = decl.parent;
+        const webpRule = originRule.cloneAfter();
+
+        webpRule.selectors = webpRule.selectors.map((i) => `html.wepb ${i}`);
+        webpRule.each((i) => {
+            if (i.prop !== decl.prop && i.value !== decl.value) {
+                i.remove();
+            } else {
+                i.value = i.value.replace(originUrl, webpUrl);
+            }
+        });
+        webpRule.raws.semicolon = true;
+        webpRule.raws.before = '\n';
+
+        return originUrl;
     },
 });
