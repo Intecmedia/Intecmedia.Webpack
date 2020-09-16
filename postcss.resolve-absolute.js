cc222778af5661d426239be53011b86e326213a0
dkrnl <dkrnl@yandex.ru>
2020-09-16 15:30:26 +0700

    .

--- postcss.resolve-absolute.js
+++ postcss.resolve-absolute.js
@@ -9,22 +9,26 @@ const logger = weblog({ name: 'resolve-absolute' });
 const ABSOLUTE_PATTERN = /^\/([^/])/;
 const ABSOLUTE_REPLACE = '../$1';
 
-module.exports = ({ silent }) => postcssUrl({
-    url(asset, dir, options, decl, warn) {
-        const { originUrl } = asset;
+module.exports = ({ silent }) => {
+    const plugin = postcssUrl({
+        url(asset, dir, options, decl, warn) {
+            const { originUrl } = asset;
 
-        if (!ABSOLUTE_PATTERN.test(originUrl)) return originUrl;
+            if (!ABSOLUTE_PATTERN.test(originUrl)) return originUrl;
 
-        const resolvedUrl = originUrl.replace(ABSOLUTE_PATTERN, ABSOLUTE_REPLACE); // resolve absolute urls
-        const message = [
-            `Absolute url(${JSON.stringify(originUrl)}) resolved as url(${JSON.stringify(resolvedUrl)}).`,
-            'Please fix to relative.',
-        ].join(' ');
+            const resolvedUrl = originUrl.replace(ABSOLUTE_PATTERN, ABSOLUTE_REPLACE); // resolve absolute urls
+            const message = [
+                `Absolute url(${JSON.stringify(originUrl)}) resolved as url(${JSON.stringify(resolvedUrl)}).`,
+                'Please fix to relative.',
+            ].join(' ');
 
-        warn(message);
-        logger.error(message);
-        if (!silent) throw new Error(message);
+            warn(message);
+            logger.error(message);
+            if (!silent) throw new Error(message);
 
-        return resolvedUrl;
-    },
-});
+            return resolvedUrl;
+        },
+    });
+    plugin.postcssPlugin = 'postcss.resolve-absolute.js';
+    return plugin;
+};
--- postcss.webp.js
+++ postcss.webp.js
@@ -7,41 +7,45 @@ const postcssUrl = require('postcss-url');
 
 const IMAGE_PATTERN = /.(png|jpg|jpeg)(\?.*)?$/i;
 
-module.exports = () => postcssUrl({
-    url(asset, dir, options, decl) {
-        const { originUrl } = asset;
+module.exports = () => {
+    const plugin = postcssUrl({
+        url(asset, dir, options, decl) {
+            const { originUrl } = asset;
 
-        if (!IMAGE_PATTERN.test(originUrl)) return originUrl;
+            if (!IMAGE_PATTERN.test(originUrl)) return originUrl;
 
-        const [originRequest, originSearch = ''] = originUrl.split('?', 2);
-        const originParams = new URLSearchParams(originSearch);
-        if (originParams.get('format') === 'webp') return originUrl;
+            const [originRequest, originSearch = ''] = originUrl.split('?', 2);
+            const originParams = new URLSearchParams(originSearch);
+            if (originParams.get('format') === 'webp') return originUrl;
 
-        originParams.set('resize', '');
-        originParams.set('format', 'webp');
-        originParams.set('name', path.basename(originRequest, path.extname(originRequest)));
-        const webpUrl = [originRequest, originParams].join('?');
+            originParams.set('resize', '');
+            originParams.set('format', 'webp');
+            originParams.set('name', path.basename(originRequest, path.extname(originRequest)));
+            const webpUrl = [originRequest, originParams].join('?');
 
-        const originRule = decl.parent;
-        const webpRule = originRule.__postcssWebpRule__ || originRule.cloneAfter();
+            const originRule = decl.parent;
+            const webpRule = originRule.__postcssWebpRule__ || originRule.cloneAfter();
 
-        if (!originRule.__postcssWebpRule__) {
-            originRule.__postcssWebpRule__ = webpRule;
+            if (!originRule.__postcssWebpRule__) {
+                originRule.__postcssWebpRule__ = webpRule;
 
-            webpRule.selectors = webpRule.selectors.map((i) => `html.wepb ${i}`);
+                webpRule.selectors = webpRule.selectors.map((i) => `html.wepb ${i}`);
+                webpRule.each((i) => {
+                    if (i.prop !== decl.prop && i.value !== decl.value) {
+                        i.remove();
+                    }
+                });
+                webpRule.raws.semicolon = true;
+                webpRule.raws.before = '\n';
+            }
+
             webpRule.each((i) => {
-                if (i.prop !== decl.prop && i.value !== decl.value) {
-                    i.remove();
-                }
+                i.value = i.value.replace(originUrl, webpUrl);
             });
-            webpRule.raws.semicolon = true;
-            webpRule.raws.before = '\n';
-        }
 
-        webpRule.each((i) => {
-            i.value = i.value.replace(originUrl, webpUrl);
-        });
-
-        return originUrl;
-    },
-});
+            return originUrl;
+        },
+    });
+    plugin.postcssPlugin = 'postcss.webp.js';
+    return plugin;
+};
