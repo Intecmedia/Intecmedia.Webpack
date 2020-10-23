d010b3efd4ae362caf92e0deb0aea904d0b5040c
dkrnl <dkrnl@yandex.ru>
2020-10-23 16:00:22 +0700

    .

--- browsersync.config.js
+++ browsersync.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     files: [
         '../include/module/**/*.{php,phtml}',
-        '../include/template/**/*.{php,phtml}',
+        '../include/template/**/*.{php,phtml,svg}',
     ],
     reloadDelay: 1000,
     reloadDebounce: 1000,
