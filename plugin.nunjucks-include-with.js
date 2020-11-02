ba6858882d163974e05a17c292fbb9dfdfd18441
dkrnl <dkrnl@yandex.ru>
2020-11-02 17:12:59 +0700

    .

--- plugin.nunjucks-include-with.js
+++ plugin.nunjucks-include-with.js
@@ -2,7 +2,7 @@
 /* eslint "compat/compat": "off" */
 
 const path = require('path');
-const nunjucks = require('nunjucks');
+const nunjucksRuntime = require('nunjucks/src/runtime');
 
 const RELATIVE_PATTERN = /^\.{1,2}\//;
 
@@ -28,8 +28,8 @@ function IncludeWithExtension({ nunjucksEnv, tagName = 'includeWith' }) {
     this.run = (context, partialPath, data = {}, { useContext = true } = {}) => {
         const fullPath = RELATIVE_PATTERN.test(partialPath) ? path.resolve(this.cwd, partialPath) : partialPath;
         const mergedContext = useContext ? ({ ...context.ctx, ...data }) : data;
-        const result = this.nunjucksEnv.render(fullPath, mergedContext);
-        return new nunjucks.runtime.SafeString(result);
+        const renderResult = this.nunjucksEnv.render(fullPath, mergedContext);
+        return new nunjucksRuntime.SafeString(renderResult);
     };
 }
 
