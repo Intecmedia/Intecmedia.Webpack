365e6f5069dd7155b501ff9d0edf95103d62a30a
dkrnl <dkrnl@yandex.ru>
2021-03-15 14:04:37 +0700

    .

--- svgo.config.js
+++ svgo.config.js
@@ -1,15 +1,17 @@
 /* eslint-env node -- webpack is node env */
 /* eslint global-require: "off", "compat/compat": "off" -- webpack is node env */
 
+const SVGO = require('svgo');
 const uniqueId = require('lodash.uniqueid');
 
 const SvgoCreateConfig = (config) => ({
     js2svg: { pretty: (!!config.pretty) },
-    plugins: [
+    plugins: SVGO.extendDefaultPlugins([
         {
             name: 'cleanupIDs',
-            ...(config.prefix ? {
-                active: true,
+            active: config.prefix ? true : false,
+            params: (config.prefix ? {
+
                 prefix: {
                     toString() {
                         return uniqueId(config.prefix);
@@ -17,9 +19,7 @@ const SvgoCreateConfig = (config) => ({
                 },
                 preserve: [], // ignore ids
                 preservePrefixes: [], // ignore prefix ids
-            } : {
-                active: false,
-            }),
+            } : {}),
         },
         { name: 'convertShapeToPath', active: false },
         { name: 'convertStyleToAttrs', active: false },
@@ -28,8 +28,7 @@ const SvgoCreateConfig = (config) => ({
         { name: 'removeViewBox', active: false },
         { name: 'removeUselessDefs', active: false },
         { name: 'noDataURL', ...require('./svgo.no-data-url.js') },
-    ],
-    multipass: true,
+    ]),
 });
 
 module.exports.SvgoCreateConfig = SvgoCreateConfig;
