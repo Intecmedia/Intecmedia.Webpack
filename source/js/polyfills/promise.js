7e66ebdfb22c50a1752055fa77ea314c3ec52240
dkrnl <dkrnl@yandex.ru>
2021-04-04 22:19:34 +0700

    polyfill: add Promise.any add Promise.allSettled

--- source/js/polyfills/promise.js
+++ source/js/polyfills/promise.js
@@ -6,7 +6,18 @@ if (!window.Promise) {
     window.Promise = require('core-js/stable/promise');
 }
 
+if (window.Promise && !('any' in window.Promise.prototype)) {
+    // eslint-disable-next-line global-require -- conditinal polyfill
+    window.Promise.prototype.any = require('core-js/stable/promise/any');
+}
+
 if (window.Promise && !('finally' in window.Promise.prototype)) {
     // eslint-disable-next-line global-require -- conditinal polyfill
     window.Promise.prototype.finally = require('core-js/stable/promise/finally');
 }
+
+if (window.Promise && !('allSettled' in window.Promise.prototype)) {
+    // eslint-disable-next-line global-require -- conditinal polyfill
+    window.Promise.prototype.allSettled = require('core-js/stable/promise/all-settled');
+}
+
