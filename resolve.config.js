4e2cc98839b7f61d15844ab64448b407ebdf8fbf
dkrnl <dkrnl@yandex.ru>
2021-04-06 20:58:01 +0700

    .

--- resolve.config.js
+++ resolve.config.js
@@ -8,6 +8,7 @@ module.exports = {
     resolve: {
         alias: {
             '~': path.join(ENV.SOURCE_PATH, 'js'),
+            // jquery: require.resolve('jquery/dist/jquery.slim.js'),
         },
     },
 };
