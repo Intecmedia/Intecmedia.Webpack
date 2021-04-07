f160e323bece93cbaec46e27bfdbb0bc3d7b48dc
dkrnl <dkrnl@yandex.ru>
2021-04-07 11:43:22 +0700

    babel: remove transform runtime

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ const ENV = require('./app.env.js');
 
 module.exports = {
     plugins: [
-        ['@babel/transform-runtime', {}],
         ['@babel/plugin-proposal-optional-chaining', { loose: false }],
         ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
         ['@babel/plugin-proposal-decorators', { legacy: true }],
