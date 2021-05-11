d2df6a48c201341c2ab6d6d3d7c13f1d60fbebea
dkrnl <dkrnl@yandex.ru>
2021-05-11 11:41:32 +0700

    .

--- source/js/components/viewport-height.js
+++ source/js/components/viewport-height.js
@@ -1,11 +1,8 @@
 // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 
 const resizeHandler = () => {
-   const vh = window.innerHeight * 0.01;
-   document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-   const vw = window.innerWidth * 0.01;
-   document.documentElement.style.setProperty('--vw', `${vw}px`);
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 };
 
 window.addEventListener('resize', resizeHandler);
