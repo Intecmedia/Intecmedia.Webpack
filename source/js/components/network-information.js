997d7a1670131a37b3030fba2a59e269f1dc7a5a
dkrnl <dkrnl@yandex.ru>
2021-05-29 19:13:28 +0700

    .

--- source/js/components/network-information.js
+++ source/js/components/network-information.js
@@ -1,8 +1,8 @@
 /* global VERBOSE */
-
 const $makeNetworkClass = ($type) => ($type ? ` network-detected network-${$type}` : ' network-unknown');
 
 const $html = document.documentElement;
+
 // eslint-disable-next-line compat/compat -- we are detect vendor prefix
 const $conn = (navigator.connection || navigator.mozConnection || navigator.webkitConnection);
 
@@ -11,58 +11,58 @@ if (!$conn) {
         console.log('[network-information] unknown');
     }
     $html.classList.add('network-unknown');
-    return;
-}
+} else {
 
-// network information test: 'slow-2g', '2g', '3g', or '4g'
-let $effectiveType = $conn.effectiveType;
-$html.classList.add($makeNetworkClass($effectiveType));
-if (VERBOSE) {
-    console.log(`[network-information] set effectiveType=${$effectiveType}`);
-}
+    // network information test: 'slow-2g', '2g', '3g', or '4g'
+    let $effectiveType = $conn.effectiveType;
+    $html.classList.add($makeNetworkClass($effectiveType));
+    if (VERBOSE) {
+        console.log(`[network-information] set effectiveType=${$effectiveType}`);
+    }
 
-// network save-data test
-let $saveData = $conn.saveData;
-$html.classList.toggle('save-data', $saveData);
-$html.classList.toggle('no-save-data', !$saveData);
-if (VERBOSE) {
-    console.log(`[network-information] set saveData=${JSON.stringify($saveData)}`);
-}
+    // network save-data test
+    let $saveData = $conn.saveData;
+    $html.classList.toggle('save-data', $saveData);
+    $html.classList.toggle('no-save-data', !$saveData);
+    if (VERBOSE) {
+        console.log(`[network-information] set saveData=${JSON.stringify($saveData)}`);
+    }
 
-const $listener = () => {
+    const $listener = () => {
     // detect network type
-    if ($effectiveType !== $conn.effectiveType) {
-        if (VERBOSE) {
-            console.log('[network-information] change effectiveType:', JSON.stringify({
-                from: $effectiveType,
-                to: $conn.effectiveType,
-            }));
+        if ($effectiveType !== $conn.effectiveType) {
+            if (VERBOSE) {
+                console.log('[network-information] change effectiveType:', JSON.stringify({
+                    from: $effectiveType,
+                    to: $conn.effectiveType,
+                }));
+            }
+            $html.classList.remove($makeNetworkClass($effectiveType));
+            $html.classList.add($makeNetworkClass($conn.effectiveType));
+            $effectiveType = $conn.effectiveType;
         }
-        $html.classList.remove($makeNetworkClass($effectiveType));
-        $html.classList.add($makeNetworkClass($conn.effectiveType));
-        $effectiveType = $conn.effectiveType;
-    }
 
-    // detect save-data header
-    if ($saveData !== $conn.saveData) {
-        if (VERBOSE) {
-            console.log('[network-information] change saveData:', JSON.stringify({
-                from: $saveData,
-                to: $conn.saveData,
-            }));
+        // detect save-data header
+        if ($saveData !== $conn.saveData) {
+            if (VERBOSE) {
+                console.log('[network-information] change saveData:', JSON.stringify({
+                    from: $saveData,
+                    to: $conn.saveData,
+                }));
+            }
+            $saveData = $conn.saveData;
+            $html.classList.toggle('save-data', $conn.saveData);
+            $html.classList.toggle('no-save-data', !$conn.saveData);
         }
-        $saveData = $conn.saveData;
-        $html.classList.toggle('save-data', $conn.saveData);
-        $html.classList.toggle('no-save-data', !$conn.saveData);
-    }
-};
+    };
 
-if ($conn.addEventListener && ($conn instanceof EventTarget)) {
-    try {
-        $conn.addEventListener('change', $listener);
-    } catch (error) {
-        if (VERBOSE) {
-            console.log('[network-information] error:', error);
+    if ($conn.addEventListener && ($conn instanceof EventTarget)) {
+        try {
+            $conn.addEventListener('change', $listener);
+        } catch (error) {
+            if (VERBOSE) {
+                console.log('[network-information] error:', error);
+            }
         }
     }
 }
