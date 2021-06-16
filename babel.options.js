bffadd96884b7868945b4b6cae35e0dfc4ebc431
dkrnl <dkrnl@yandex.ru>
2021-06-16 18:40:13 +0700

    .

--- babel.options.js
+++ babel.options.js
@@ -11,6 +11,7 @@ module.exports = {
     includeTransform: [
         // enable babel transform
         /\.(ts|mjs|cjs)(\?.*)?$/i,
+        /\.module(\.min)?\.js(\?.*)?$/i,
         path.join(__dirname, 'source'),
         // path.join(__dirname, 'node_modules', 'example-package-name'),
     ],
