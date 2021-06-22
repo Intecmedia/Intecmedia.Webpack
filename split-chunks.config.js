31c4b816f86796b01154d2ac8d543110c40f031a
dkrnl <dkrnl@yandex.ru>
2021-06-22 20:32:06 +0700

    webpack: split polyfills chunks

--- split-chunks.config.js
+++ split-chunks.config.js
@@ -1,8 +1,34 @@
 /* eslint-env node -- webpack is node env */
 /* eslint "compat/compat": "off" -- webpack is node env */
 
+const path = require('path');
+const ENV = require('./app.env');
+
+const corejsDir = path.dirname(require.resolve('core-js'));
+const polyfillsDir = path.normalize(`${ENV.SOURCE_PATH}/js/polyfills`);
+
 module.exports = {
     cacheGroups: {
+        polyfills: {
+            chunks: 'initial',
+            enforce: true,
+            test: ({ resource, resourceResolveData }) => {
+                if ((
+                    resourceResolveData
+                    && resourceResolveData.context
+                    && resourceResolveData.context.issuer
+                    && resourceResolveData.context.issuer.indexOf(polyfillsDir) === 0
+                ) || (
+                    resource
+                    && resource.indexOf(corejsDir) === 0
+                )) {
+                    return true;
+                }
+                return false;
+            },
+            name: 'vendor.polyfills',
+            priority: 10,
+        },
         sentry: {
             chunks: 'all',
             enforce: true,
