b699b7a9167bb76b9154fbf6d23b6f1bf7ddbcfc
dkrnl <dkrnl@yandex.ru>
2021-06-23 17:43:09 +0700

    remove polyfill chunks

--- split-chunks.config.js
+++ split-chunks.config.js
@@ -1,39 +1,8 @@
 /* eslint-env node -- webpack is node env */
 /* eslint "compat/compat": "off" -- webpack is node env */
 
-const path = require('path');
-const ENV = require('./app.env');
-
-const corejsDir = path.dirname(require.resolve('core-js'));
-const polyfillsDir = path.normalize(`${ENV.SOURCE_PATH}/js/polyfills/`);
-const polyfillsEntry = path.normalize(`${ENV.SOURCE_PATH}/js/polyfills.js`);
-
 module.exports = {
     cacheGroups: {
-        polyfills: {
-            chunks: 'initial',
-            enforce: true,
-            test: ({ resource, resourceResolveData }) => {
-                if ((
-                    resourceResolveData
-                    && resourceResolveData.context
-                    && resourceResolveData.context.issuer
-                    && resourceResolveData.context.issuer.indexOf(polyfillsDir) === 0
-                ) || (
-                    resource
-                    && (
-                        resource.indexOf(corejsDir) === 0
-                        || resource.indexOf(polyfillsEntry) === 0
-                        || resource.indexOf(polyfillsDir) === 0
-                    )
-                )) {
-                    return true;
-                }
-                return false;
-            },
-            name: 'vendor.polyfills',
-            priority: 10,
-        },
         sentry: {
             chunks: 'all',
             enforce: true,
