0c5e6daf7f6ac3d3b665446a10b52533666b9803
dkrnl <dkrnl@yandex.ru>
2021-07-02 12:29:05 +0700

    .

--- source/js/polyfills/focus-within.js
+++ source/js/polyfills/focus-within.js
@@ -1,6 +1,5 @@
 // for polyfill use only require
 const { default: focusWithin } = require('focus-within');
 
-document.addEventListener('DOMContentLoaded', () => {
-    focusWithin(document);
-});
+focusWithin(document);
+
