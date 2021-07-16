53c739e3d93c4e87c4040511b8a83dee5d3ebd1a
dkrnl <dkrnl@yandex.ru>
2021-07-16 12:05:50 +0700

    .

---
+++ source/js/utils/transform-translate.js
@@ -0,0 +1,14 @@
+export default function getTransformTranslate(element) {
+    const translate = { x: 0, y: 0 };
+    const style = window.getComputedStyle(element);
+    const [, matrixType, matrixValue] = style.transform.match(/^(matrix|matrix3d)\((.+)\)$/);
+    const matrixArray = matrixValue.split(', ');
+    if (matrixType === 'matrix3d') {
+        translate.x = parseFloat(matrixArray[12]) || 0;
+        translate.y = parseFloat(matrixArray[13]) || 0;
+    } else {
+        translate.x = parseFloat(matrixArray[4]) || 0;
+        translate.y = parseFloat(matrixArray[5]) || 0;
+    }
+    return translate;
+}
