1ac8d3ffd32138189b0f342fd63c51defa9cc447
dkrnl <dkrnl@yandex.ru>
2021-07-16 13:04:42 +0700

    .

--- source/js/utils/visible-ratio.js
+++ source/js/utils/visible-ratio.js
@@ -1,19 +1,19 @@
 import getTransformTranslate from '~/utils/transform-translate';
 
 export function visibleRatioVertical(element, scrollTop = null, scrollHeight = null) {
-    const translate = getTransformTranslate(element);
     const top = scrollTop || document.documentElement.scrollTop;
     const height = scrollHeight || window.innerHeight;
-    const distance = (top + height) - (element.offsetTop + translate.y);
+    const translateY = getTransformTranslate(element).y;
+    const distance = (top + height) - (element.offsetTop + translateY);
     const ratio = Number(distance / (height + element.offsetHeight)).toFixed(4);
     return Math.min(1, Math.max(0, ratio));
 }
 
 export function visibleRatioHorizontal(element, scrollLeft = null, scrollWidth = null) {
-    const translate = getTransformTranslate(element);
     const left = scrollLeft || document.documentElement.scrollLeft;
     const width = scrollWidth || window.innerWidth;
-    const distance = (left + width) - (element.offsetLeft + translate.x);
+    const translateX = getTransformTranslate(element).x;
+    const distance = (left + width) - (element.offsetLeft + translateX);
     const ratio = Number(distance / (width + element.offsetWidth)).toFixed(4);
     return Math.min(1, Math.max(0, ratio));
 }
