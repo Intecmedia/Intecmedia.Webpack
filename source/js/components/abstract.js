0751dda319023cc6ed34ec53110e4f3e7e49a836
dkrnl <dkrnl@yandex.ru>
2021-07-20 13:40:16 +0700

    .

--- source/js/components/abstract.js
+++ source/js/components/abstract.js
@@ -16,17 +16,17 @@ export default class AbstractComponent {
     }
 
     on(type, listener, options = {}) {
-        const name = `${type}.component`;
+        const name = `${type}.${this.options.name}`;
         return this.options.element.addEventListener(name, listener, options);
     }
 
     off(type, listener, options = {}) {
-        const name = `${type}.component`;
+        const name = `${type}.${this.options.name}`;
         return this.options.element.removeEventListener(name, listener, options);
     }
 
     trigger(type, options = {}) {
-        const name = `${type}.component`;
+        const name = `${type}.${this.options.name}`;
         const event = new CustomEvent(name, options);
         return this.options.element.dispatchEvent(event);
     }
