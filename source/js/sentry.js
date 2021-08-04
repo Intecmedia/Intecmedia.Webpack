7039498be35894ea32519a27ce4399ba3d3b588d
dkrnl <dkrnl@yandex.ru>
2021-08-04 12:49:21 +0700

    .

--- source/js/sentry.js
+++ source/js/sentry.js
@@ -1,32 +1,33 @@
 /* global NODE_ENV APP DEBUG VERBOSE */
-if ((NODE_ENV === 'production' || DEBUG) && APP.SENTRY.dsn) {
-    const sentryCheckIgnore = (event) => {
-        // Detect if we got a ReportingObserver event
-        if (event?.message?.indexOf('ReportingObserver') === 0) {
-            // And check whether sourceFile points to chrome-extension.
-            if (event?.extra?.body?.sourceFile?.indexOf('chrome-extension') === 0) {
-                return true;
-            }
+
+const sentryCheckIgnore = (event) => {
+    // Detect if we got a ReportingObserver event
+    if (event?.message?.indexOf('ReportingObserver') === 0) {
+        // And check whether sourceFile points to chrome-extension.
+        if (event?.extra?.body?.sourceFile?.indexOf('chrome-extension') === 0) {
+            return true;
         }
-        return false;
-    };
+    }
+    return false;
+};
 
-    const Sentry = await import(/* webpackChunkName: "vendor.sentry" */'@sentry/browser');
-
-    Sentry.init({
-        debug: DEBUG || false,
-        dsn: APP.SENTRY.dsn,
-        beforeSend(event) {
-            if (sentryCheckIgnore(event)) {
-                if (VERBOSE) {
-                    console.log('[sentry]', event);
+if ((NODE_ENV === 'production' || DEBUG) && APP.SENTRY.dsn) {
+    import(/* webpackChunkName: "vendor.sentry" */'@sentry/browser').then(({ init }) => {
+        init({
+            debug: DEBUG || false,
+            dsn: APP.SENTRY.dsn,
+            beforeSend(event) {
+                if (sentryCheckIgnore(event)) {
+                    if (VERBOSE) {
+                        console.log('[sentry]', event);
+                    }
+                    return null;
                 }
-                return null;
-            }
-            return event;
-        },
-        ignoreErrors: APP.SENTRY.ignoreErrors || [],
-        blacklistUrls: APP.SENTRY.blacklistUrls || [],
-        whitelistUrls: APP.SENTRY.whitelistUrls || [],
+                return event;
+            },
+            ignoreErrors: APP.SENTRY.ignoreErrors || [],
+            blacklistUrls: APP.SENTRY.blacklistUrls || [],
+            whitelistUrls: APP.SENTRY.whitelistUrls || [],
+        });
     });
 }
