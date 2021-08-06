98a11980e93119ea1f79ccec439ee72b7ef4d0dd
dkrnl <dkrnl@yandex.ru>
2021-08-06 15:56:05 +0700

    .

--- source/js/components/aquilon-validator.js
+++ source/js/components/aquilon-validator.js
@@ -3,14 +3,14 @@ import nextTick from '~/utils/next-tick';
 async function updateValidator() {
     await nextTick(); // wait side effects changes
 
-    if (!$.aquilonValidator.initNode) {
+    if (!window.$.aquilonValidator.initNode) {
         throw new Error('[aquilon-validator] Method `initNode` does not exist. Please update.');
     }
 
-    $.aquilonValidator.initNode(document);
+    window.$.aquilonValidator.initNode(document);
 }
 
-if ($.aquilonValidator) {
+if (window.$.aquilonValidator) {
     // SPA events
     window.addEventListener('pushstate', updateValidator, false);
     window.addEventListener('popstate', updateValidator, false);
