5c50642265f48bd00ed9dde65bbe1a28470beb93
dkrnl <dkrnl@yandex.ru>
2021-08-26 11:22:18 +0700

    svgo: remove deprecated `extendDefaultPlugins` (see: https://github.com/svg/svgo/releases/tag/v2.4.0)

--- svgo.config.js
+++ svgo.config.js
@@ -1,34 +1,38 @@
 /* eslint-env node -- webpack is node env */
 /* eslint global-require: "off", "compat/compat": "off" -- webpack is node env */
 
-const SVGO = require('svgo');
 const uniqueId = require('lodash.uniqueid');
 
 const SvgoCreateConfig = (config) => ({
     js2svg: { pretty: (!!config.pretty) },
-    plugins: SVGO.extendDefaultPlugins([
+    plugins: [
         {
-            name: 'cleanupIDs',
-            active: !!config.prefix,
-            params: (config.prefix ? {
-
-                prefix: {
-                    toString() {
-                        return uniqueId(config.prefix);
-                    },
+            name: 'preset-default',
+            params: {
+                overrides: {
+                    // customize options for plugins included in preset
+                    cleanupIDs: (config.prefix ? {
+                        prefix: {
+                            toString() {
+                                return uniqueId(config.prefix);
+                            },
+                        },
+                        preserve: [], // ignore ids
+                        preservePrefixes: [], // ignore prefix ids
+                    } : false),
+                    // or disable plugins
+                    convertShapeToPath: false,
+                    convertStyleToAttrs: false,
+                    convertTransform: false,
+                    removeDimensions: false,
+                    removeViewBox: false,
+                    removeUselessDefs: false,
                 },
-                preserve: [], // ignore ids
-                preservePrefixes: [], // ignore prefix ids
-            } : {}),
+            },
         },
-        { name: 'convertShapeToPath', active: false },
-        { name: 'convertStyleToAttrs', active: false },
-        { name: 'convertTransform', active: false },
-        { name: 'removeDimensions', active: false },
-        { name: 'removeViewBox', active: false },
-        { name: 'removeUselessDefs', active: false },
+        // configure builtin plugin not included in preset
         { name: 'noDataURL', ...require('./svgo.no-data-url') },
-    ]),
+    ],
 });
 
 module.exports.SvgoCreateConfig = SvgoCreateConfig;
