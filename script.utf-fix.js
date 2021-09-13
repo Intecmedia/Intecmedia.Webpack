2030d2f3da1ebe7c6c5973745bdab86bee8c87ef
dkrnl <dkrnl@yandex.ru>
2021-09-13 15:32:32 +0700

    .

--- script.utf-fix.js
+++ script.utf-fix.js
@@ -10,6 +10,13 @@ const logger = weblog({ name: 'utf-fix' });
 const ENV = require('./app.env');
 const UTILS = require('./webpack.utils');
 
+function stripBom(string) {
+    if (string.charCodeAt(0) === 0xFEFF) {
+        return string.slice(1);
+    }
+    return string;
+}
+
 UTILS.globArray([
     path.join(__dirname, '*.{js,json}'),
     `${ENV.SOURCE_PATH}/**/*.{html,svg,js,json,css,scss}`,
@@ -24,7 +31,7 @@ UTILS.globArray([
 
         const relativePath = slash(path.relative(__dirname, resourcePath));
         const source = fs.readFileSync(resourcePath, 'utf8').toString();
-        const fixedSource = source.normalize('NFC')
+        const fixedSource = stripBom(source.normalize('NFC'))
             .replace(/\r\n/g, '\n')
             .replace(/\t/g, '    ')
             .replace(/[ \t]+\n/g, '\n');
