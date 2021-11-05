5f6bbb479d37dd64eba0b6ec46c573b01b7477d3
dkrnl <dkrnl@yandex.ru>
2021-11-05 14:13:55 +0700

    remove legacy

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,10 +82,7 @@ module.exports = {
         },
         'polyfills': [
             'fetch',
-            'Array.from',
             'IntersectionObserver',
-            'Object.assign',
-            'Object.values',
             'Promise',
             'ResizeObserver',
             'Symbol',
