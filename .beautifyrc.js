fa8a9b725d9cd9293d899e099ec557972e8adcb6
dkrnl <dkrnl@yandex.ru>
2022-01-15 19:37:51 +0700

    .

--- .beautifyrc.js
+++ .beautifyrc.js
@@ -1,12 +1,14 @@
 /* eslint-env node -- webpack is node env */
-/* eslint "compat/compat": "off" -- webpack is node env */
-/* eslint "quote-props": ["error", "always"] -- more readability keys */
+/* eslint 'compat/compat': 'off' -- webpack is node env */
+/* eslint 'quote-props': ['error', 'always'] -- more readability keys */
 
 module.exports = {
     'indent_char': ' ',
     'indent_size': 4,
     'html': {
-        'wrap_line_length': 120,
         'max_preserve_newlines': 1,
+        'wrap_line_length': 120,
+        'wrap_attributes': 'preserve-aligned',
+        'wrap_attributes_indent_size': 4,
     },
 };
