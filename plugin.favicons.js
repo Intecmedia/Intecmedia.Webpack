a8b14ef4618e62bbfd991f7c1599f41f29d775ff
dkrnl <dkrnl@yandex.ru>
2022-01-21 12:14:26 +0700

    favicons: more readable errors

--- plugin.favicons.js
+++ plugin.favicons.js
@@ -63,9 +63,9 @@ module.exports.FavIcon = function FavIcon(options) {
     const mergedOptions = deepMerge({}, DEFAULT_FAVICON, options);
     const logoSize = ImageSize(mergedOptions.logo);
     if (!(logoSize && logoSize.type === 'png')) {
-        throw new Error(`FavIcon '${mergedOptions.logo}': the file is not a valid image`);
+        throw new Error(`FavIcon '${mergedOptions.logo}': the file is not a valid image (allowed only png).`);
     } else if (!(logoSize.width === 64 && logoSize.height === 64)) {
-        throw new Error(`FavIcon '${mergedOptions.logo}': image size is not than (64 x 64)`);
+        throw new Error(`FavIcon '${mergedOptions.logo}': image size (${logoSize.width}x${logoSize.height}) is not than (64x64).`);
     }
     return new FaviconsWebpackPlugin(fixWebpackConfig(mergedOptions));
 };
@@ -104,9 +104,9 @@ module.exports.AppIcon = function AppIcon(options) {
     const mergedOptions = deepMerge({}, DEFAULT_APPICON, options);
     const logoSize = ImageSize(mergedOptions.logo);
     if (!(logoSize && logoSize.type === 'png')) {
-        throw new Error(`AppIcon '${mergedOptions.logo}': the file is not a valid image`);
+        throw new Error(`AppIcon '${mergedOptions.logo}': the file is not a valid image (allowed only png).`);
     } else if (!(logoSize.width === 1024 && logoSize.height === 1024)) {
-        throw new Error(`AppIcon '${mergedOptions.logo}': image size is not than (1024 x 1024)`);
+        throw new Error(`AppIcon '${mergedOptions.logo}': image size (${logoSize.width}x${logoSize.height}) is not than (1024x1024).`);
     }
     return new FaviconsWebpackPluginOriginal(fixWebpackConfig(mergedOptions));
 };
