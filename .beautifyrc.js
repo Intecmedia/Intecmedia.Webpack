8f8d687cf3e6a700fad3392ebf8db1bdaf64735d
dkrnl <dkrnl@yandex.ru>
2022-01-25 16:06:21 +0700

    feat(format): disable `max_preserve_newlines` option

--- .beautifyrc.js
+++ .beautifyrc.js
@@ -6,7 +6,7 @@ module.exports = {
     'indent_char': ' ',
     'indent_size': 4,
     'html': {
-        'max_preserve_newlines': 1,
+        'max_preserve_newlines': -1,
         'wrap_line_length': 120,
         'wrap_attributes': 'preserve-aligned',
         'wrap_attributes_indent_size': 4,
