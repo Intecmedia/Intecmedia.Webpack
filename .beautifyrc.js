e166fb44787d0b60f23609039e40e7faaa729fed
dkrnl <dkrnl@yandex.ru>
2022-01-25 17:48:49 +0700

    feat(format): add `inline` option

--- .beautifyrc.js
+++ .beautifyrc.js
@@ -6,9 +6,17 @@ module.exports = {
     'indent_char': ' ',
     'indent_size': 4,
     'html': {
-        'max_preserve_newlines': -1,
+        'max_preserve_newlines': 1,
         'wrap_line_length': 120,
         'wrap_attributes': 'preserve-aligned',
         'wrap_attributes_indent_size': 4,
+        'inline': [
+            'abbr', 'b', 'bdi', 'bdo', 'br', 'cite',
+            'del', 'dfn', 'em', 'i',
+            'ins', 'kbd', 'mark', 'meter',
+            'q', 's', 'samp', 'small',
+            'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr', 'text',
+            'acronym', 'big', 'strike', 'tt',
+        ],
     },
 };
