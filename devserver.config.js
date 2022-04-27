55ddb03de434be712c1d29b3687711dfe354da13
dkrnl <dkrnl@yandex.ru>
2022-04-27 14:08:50 +0700

    feat: `historyApiFallback` exaple

--- devserver.config.js
+++ devserver.config.js
@@ -4,4 +4,9 @@ module.exports = {
     watchFiles: [
         // '../include/template/**/*.{php,phtml,svg}',
     ],
+    historyApiFallback: {
+        rewrites: [
+            // { from: /^\/example\/(.+)/, to: '/example/index.html' },
+        ],
+    },
 };
