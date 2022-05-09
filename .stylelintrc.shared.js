a9aae9116abe6ea584850eb74f5d279441446598
dkrnl <dkrnl@yandex.ru>
2022-05-09 09:21:01 +0700

    fix: allow declaration-block-no-duplicate-custom-properties

--- .stylelintrc.shared.js
+++ .stylelintrc.shared.js
@@ -12,7 +12,7 @@ module.exports = {
     ],
     'rules': {
         'color-hex-alpha': 'never',
-        'declaration-block-no-duplicate-custom-properties': true,
+        'declaration-block-no-duplicate-custom-properties': null,
         'declaration-block-no-duplicate-properties': [true, {
             'ignore': [
                 'consecutive-duplicates-with-different-values',
