762eb6ee9fccdb65386cf45f8ec51963e2fc2073
dkrnl <dkrnl@yandex.ru>
2022-07-20 10:02:02 +0700

    fix: clean script

--- script.clean.js
+++ script.clean.js
@@ -10,12 +10,14 @@ const logger = weblog({ name: 'clean' });
 const ENV = require('./app.env');
 const UTILS = require('./webpack.utils');
 
+const KEEP_PATTERN = /(\.gitkeep|thumbs)/;
+
 UTILS.glob(`${ENV.OUTPUT_PATH}/**/*`, {
     nodir: true,
     dot: true,
 }).then((files) => {
     files.forEach((filepath) => {
-        if (path.basename(filepath) === '.gitkeep') return;
+        if (KEEP_PATTERN.test(filepath)) return;
 
         fs.unlink(filepath, (unlinkError) => {
             if (unlinkError) {
