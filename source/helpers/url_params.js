d9709c7be0c31bece03a90e725f4d147705a4f06
dkrnl <dkrnl@yandex.ru>
2022-08-01 15:10:20 +0700

    feat: add url_params helper

---
+++ source/helpers/url_params.js
@@ -0,0 +1,6 @@
+/* eslint-env node -- webpack is node env */
+/* eslint "compat/compat": "off" -- webpack is node env */
+
+module.exports = function helper(params) {
+    return (new URLSearchParams(params)).toString();
+};
