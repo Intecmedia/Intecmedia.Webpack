5d1f91d56efa781314e5cb55b43f74f672feed8e
dkrnl <dkrnl@yandex.ru>
2022-08-09 12:16:26 +0700

    .

--- source/helpers/spaceless.js
+++ source/helpers/spaceless.js
@@ -1,8 +1,10 @@
 /* eslint-env node -- webpack is node env */
 /* eslint "compat/compat": "off" -- webpack is node env */
 
+const nunjucksRuntime = require('nunjucks/src/runtime');
+
 const SPACELESS_PATTERN = />\s+</g;
 
-module.exports = function helper(string) {
-    return string.replace(SPACELESS_PATTERN, '><').trim();
+module.exports = function helper(str) {
+    return nunjucksRuntime.copySafeness(str, str.replace(SPACELESS_PATTERN, '><').trim());
 };
