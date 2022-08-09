d2c2575711ee5ffddff3a127a5769cff6b1b3655
dkrnl <dkrnl@yandex.ru>
2022-08-09 17:37:11 +0700

    .

--- source/helpers/nl2br.js
+++ source/helpers/nl2br.js
@@ -3,9 +3,11 @@
 
 const nunjucksRuntime = require('nunjucks/src/runtime');
 
+const NEWLINE_PATTERN = /\r\n|\n/g;
+
 module.exports = function helper(str) {
     if (str === null || str === undefined) {
         return '';
     }
-    return nunjucksRuntime.markSafe(str.replace(/\r\n|\n/g, '<br>\n'));
+    return nunjucksRuntime.markSafe(str.replace(NEWLINE_PATTERN, '<br>\n'));
 };
