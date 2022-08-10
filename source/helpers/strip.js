222ec78b5f69b4d087dc55e69392e739e6375d0b
dkrnl <dkrnl@yandex.ru>
2022-08-10 14:46:12 +0700

    .

--- source/helpers/strip.js
+++ source/helpers/strip.js
@@ -5,7 +5,7 @@ const nunjucksRuntime = require('nunjucks/src/runtime');
 
 const STRIP_PATTERN = /\s+/g;
 
-module.exports = function helper(str, safe = true) {
+module.exports = function helper(str, safe = false) {
     const result = str.replace(STRIP_PATTERN, ' ').trim();
     return safe ? nunjucksRuntime.markSafe(result) : result;
 };
