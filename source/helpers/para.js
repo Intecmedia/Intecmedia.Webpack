0443217f478eba8c3adeecc953ac0b3b2b6bf925
dkrnl <dkrnl@yandex.ru>
2022-08-22 12:58:20 +0700

    .

--- source/helpers/para.js
+++ source/helpers/para.js
@@ -3,7 +3,7 @@
 
 const nunjucksRuntime = require('nunjucks/src/runtime');
 
-const NEWLINE_PATTERN = /(\r\n|\n)+/g;
+const NEWLINE_PATTERN = /(\r\n|\n){2,}/g;
 
 module.exports = function helper(str, open = '<p>', close = '</p>') {
     if (str === null || str === undefined) {
