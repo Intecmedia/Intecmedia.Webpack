a215fcfa332faff99e23b6f50a29b263cbca4e1e
dkrnl <dkrnl@yandex.ru>
2022-08-22 12:44:54 +0700

    feat: add paragraph helper

---
+++ source/helpers/para.js
@@ -0,0 +1,13 @@
+/* eslint-env node -- webpack is node env */
+/* eslint "compat/compat": "off" -- webpack is node env */
+
+const nunjucksRuntime = require('nunjucks/src/runtime');
+
+const NEWLINE_PATTERN = /(\r\n|\n)+/g;
+
+module.exports = function helper(str, open = '<p>', close = '</p>') {
+    if (str === null || str === undefined) {
+        return '';
+    }
+    return nunjucksRuntime.markSafe(open + str.replace(NEWLINE_PATTERN, close + open) + close);
+};
