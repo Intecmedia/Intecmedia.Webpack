f102af71ce29a59297c0c425801282ac14fb8b1f
dkrnl <dkrnl@yandex.ru>
2022-08-22 12:50:47 +0700

    .

--- source/helpers/para.js
+++ source/helpers/para.js
@@ -9,5 +9,7 @@ module.exports = function helper(str, open = '<p>', close = '</p>') {
     if (str === null || str === undefined) {
         return '';
     }
-    return nunjucksRuntime.markSafe(open + str.trim().replace(NEWLINE_PATTERN, close + open) + close);
+    return nunjucksRuntime.markSafe(
+        open + str.trim().replace(NEWLINE_PATTERN, close + open) + close,
+    );
 };
