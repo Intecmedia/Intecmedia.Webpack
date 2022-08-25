24fc56a50b5b9b8f78613395cd86373b81e1c550
dkrnl <dkrnl@yandex.ru>
2022-08-25 16:01:13 +0700

    feat: filename lint

---
+++ script.filename-lint.js
@@ -0,0 +1,35 @@
+/* eslint-env node -- webpack is node env */
+/* eslint max-len: "off", "compat/compat": "off" -- webpack is node env */
+
+const path = require('path');
+const slash = require('slash');
+const weblog = require('webpack-log');
+
+const UTILS = require('./webpack.utils');
+const config = require('./.filenamelintrc.json');
+
+const logger = weblog({ name: 'filename-lint' });
+const statMessages = { skipped: 0, failed: 0 };
+
+config.rules.forEach((rule) => {
+    const files = UTILS.globSync(rule.glob, {
+        ignore: rule.ignore,
+        nodir: true,
+    });
+    const pattern = new RegExp(rule.pattern);
+
+    files.forEach((resourcePath) => {
+        const relativePath = slash(path.relative(__dirname, resourcePath));
+        const filename = path.basename(resourcePath);
+
+        if (pattern.test(filename)) {
+            statMessages.skipped += 1;
+        } else {
+            logger.error(`Expected file name "${relativePath}" to match pattern "${rule.pattern}"`);
+            statMessages.failed += 1;
+        }
+    });
+});
+
+console.log('');
+logger.info('stats:', statMessages);
