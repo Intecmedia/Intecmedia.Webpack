e3201f05a204a8ffd66ee9b12462e3da3cc7131e
dkrnl <dkrnl@yandex.ru>
2022-09-07 08:13:05 +0700

    fix: code style

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,8 +4,36 @@
 /* eslint "sort-keys": "error" -- more readability keys */
 
 module.exports = {
+    'overrides': [
+        {
+            'files': '*.scss',
+            'options': {
+                'singleQuote': false,
+            },
+            'parser': 'postcss-scss',
+        },
+        {
+            'files': '*.js',
+            'options': {
+                'singleQuote': true,
+            },
+            'parser': '@babel/eslint-parser',
+            'parserOptions': {
+                'babelOptions': {
+                    'configFile': './babel.config.js',
+                },
+            },
+        },
+        {
+            'files': '*.ts',
+            'options': {
+                'singleQuote': true,
+            },
+            'parser': '@typescript-eslint/parser',
+        },
+    ],
     'printWidth': 120,
+    'quoteProps': 'preserve',
     'tabWidth': 4,
     'useTabs': false,
-    'quoteProps': 'preserve',
 };
