b5879ccbcd059dd0776c72ecd6512d9207778782
dkrnl <dkrnl@yandex.ru>
2022-09-15 14:56:17 +0700

    fix: lint staged tasks

--- source/js/webpack-internals.js
+++ source/js/webpack-internals.js
@@ -3,7 +3,7 @@
 /* eslint-disable-next-line camelcase, no-undef -- set webpack public path on the fly https://webpack.js.org/guides/public-path/#set-value-on-the-fly */
 __webpack_public_path__ = document.documentElement.getAttribute('data-public-path') || APP.PUBLIC_PATH;
 
-const useCompression = (NODE_ENV === 'production' || DEBUG);
+const useCompression = NODE_ENV === 'production' || DEBUG;
 
 /* eslint-disable-next-line camelcase, no-undef -- set chunk filename https://webpack.js.org/api/module-variables/#__webpack_get_script_filename__-webpack-specific */
 const org_get_script_filename = __webpack_get_script_filename__;
@@ -12,14 +12,12 @@ __webpack_get_script_filename__ = (chunkId) => {
     const filename = org_get_script_filename(chunkId);
     const [filebase, filequery] = filename.split('?', 2);
 
-    const useBrotli =
-        useCompression && APP.BROTLI && document.currentScript?.src?.match(/\.min\.js\.br/);
+    const useBrotli = useCompression && APP.BROTLI && document.currentScript?.src?.match(/\.min\.js\.br/);
     if (useBrotli) {
         return `${filebase}.br?${filequery}`;
     }
 
-    const useGzip =
-        useCompression && APP.GZIP && document.currentScript?.src?.match(/\.min\.js\.gz/);
+    const useGzip = useCompression && APP.GZIP && document.currentScript?.src?.match(/\.min\.js\.gz/);
     if (useGzip) {
         return `${filebase}.gz?${filequery}`;
     }
