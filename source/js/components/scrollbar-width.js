8b83f451e773f150b3c9590b8d5fe06054789782
dkrnl <dkrnl@yandex.ru>
2022-10-04 12:33:37 +0700

    refactor: scrollbar width detect

--- source/js/components/scrollbar-width.js
+++ source/js/components/scrollbar-width.js
@@ -1,7 +1,4 @@
-const CLASS_NAME_OUTER = 'scrollbar-width';
-const CLASS_NAME_INNER = 'scrollbar-width__inner';
-
-const CONTAINER_WIDTH = 100;
+import ScrollBarHelper from 'bootstrap/js/dist/util/scrollbar';
 
 class ScrollbarWidth {
     constructor() {
@@ -10,20 +7,7 @@ class ScrollbarWidth {
     }
 
     init() {
-        this.domOuter = document.createElement('div');
-        this.domOuter.className = CLASS_NAME_OUTER;
-        this.domOuter.style.zIndex = '-9999';
-        this.domOuter.style.position = 'absolute';
-        this.domOuter.style.visibility = 'hidden';
-        this.domOuter.style.width = `${CONTAINER_WIDTH}px`;
-        this.domOuter.style.marginLeft = `-${CONTAINER_WIDTH}px`;
-        this.domOuter.style.overflow = 'scroll';
-        document.body.appendChild(this.domOuter);
-
-        this.domInner = document.createElement('div');
-        this.domInner.className = CLASS_NAME_INNER;
-        this.domInner.style.width = '100%';
-        this.domOuter.appendChild(this.domInner);
+        this.helper = new ScrollBarHelper();
 
         window.addEventListener('resize', this.onResize);
         window.addEventListener('pushstate', this.onUpdate);
@@ -41,7 +25,7 @@ class ScrollbarWidth {
     }
 
     updateWidth() {
-        this.width = CONTAINER_WIDTH - this.domInner.offsetWidth;
+        this.width = this.helper.getWidth();
         if (this.widthLast !== this.width) {
             document.documentElement.style.setProperty('--scrollbar-width', `${this.width}px`);
             this.widthLast = this.width;
