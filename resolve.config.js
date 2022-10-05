3f20aee1f5ba1542f411ad07d054d9d4d9e1dce2
dkrnl <dkrnl@yandex.ru>
2022-10-05 12:59:20 +0700

    fix: bootstap resolve alias

--- resolve.config.js
+++ resolve.config.js
@@ -5,6 +5,7 @@ module.exports = {
     resolve: {
         alias: {
             '~': path.join(ENV.SOURCE_PATH, 'js'),
+            'bootstrap/js/src': path.join(__dirname, 'node_modules', 'bootstrap/js/dist'),
             // jquery: require.resolve('jquery/dist/jquery.slim.js'),
             // three: require.resolve('three/src/Three.js'),
         },
