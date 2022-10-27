bed984d95f20310b9bc3b7ee431aa8dfce6dce79
dkrnl <dkrnl@yandex.ru>
2022-10-27 15:59:47 +0700

    .

--- source/helpers/image_size.js
+++ source/helpers/image_size.js
@@ -8,9 +8,11 @@ module.exports = function helper(filename, nocache = false) {
     this.loaderContext.addDependency(fullpath);
     if (!(fullpath in cacheMap) || nocache) {
         cacheMap[fullpath] = ImageSize(fullpath);
+        cacheMap[fullpath].intrinsicsize = `${cacheMap[fullpath].width}x${cacheMap[fullpath].height}`;
+        if (cacheMap[fullpath].type === 'jpg') {
+            cacheMap[fullpath].type = 'jpeg';
+        }
     }
-    if (cacheMap[fullpath].type === 'jpg') {
-        cacheMap[fullpath].type = 'jpeg';
-    }
+
     return cacheMap[fullpath];
 };
