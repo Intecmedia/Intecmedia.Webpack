e3d37a7d2e319fc0beb2948cd8491a0dfc6fffe6
dkrnl <dkrnl@yandex.ru>
2022-11-10 16:10:25 +0700

    .

--- source/helpers/object_delkey.js
+++ source/helpers/object_delkey.js
@@ -1,7 +1,5 @@
 module.exports = function helper(obj, key) {
     const result = { ...obj };
-    if (key in result) {
-        delete result[key];
-    }
+    delete result[key];
     return result;
 };
