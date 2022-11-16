f3b17b44b687b02f4b23ba84fe80b078f3835411
dkrnl <dkrnl@yandex.ru>
2022-11-16 13:25:10 +0700

    deps: update html-validate plugin

--- plugin.html-validate.js
+++ plugin.html-validate.js
@@ -1,3 +1,5 @@
+const { definePlugin } = require('html-validate');
+
 const { rules: imgRules } = require('./plugin.html-validate.img');
 const { rules: bootstrapRules } = require('./plugin.html-validate.bootstrap');
 const { rules: iframeRules } = require('./plugin.html-validate.iframe');
@@ -7,7 +9,7 @@ const { rules: bemRules } = require('./plugin.html-validate.bem');
 const { rules: tableRules } = require('./plugin.html-validate.table');
 const { rules: wysiwygRules } = require('./plugin.html-validate.wysiwyg');
 
-module.exports = {
+module.exports = definePlugin({
     name: 'pitcher',
     rules: {
         ...commonRules,
@@ -19,4 +21,4 @@ module.exports = {
         ...tableRules,
         ...wysiwygRules,
     },
-};
+});
