2677411611cde15e9f706023f364687a1d3919e4
dima p <dkrnl@yandex.ru>
2023-04-11 14:09:24 +0700

    fix: event submitter polyfill

--- source/js/polyfills/event-submitter.js
+++ source/js/polyfills/event-submitter.js
@@ -1,4 +1,7 @@
 /* eslint 'compat/compat': 'off' -- useless for polyfill */
 
+// eslint-disable-next-line no-eval -- conditinal polyfill
+const polyfillRequire = (moduleId) => eval('require')(moduleId);
+
 // for polyfill use only require
-require('event-submitter-polyfill');
+polyfillRequire('event-submitter-polyfill');
