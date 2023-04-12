486039d639fb002a7e510eb62859d75367f8b325
dima p <dkrnl@yandex.ru>
2023-04-12 11:40:31 +0700

    fix: event submitter polyfill

--- source/js/polyfills/event-submitter.js
+++ source/js/polyfills/event-submitter.js
@@ -1,7 +1,36 @@
 /* eslint 'compat/compat': 'off' -- useless for polyfill */
 
-// eslint-disable-next-line no-eval -- conditinal polyfill
-const polyfillRequire = (moduleId) => eval('require')(moduleId);
+const buttonsSelector = 'button, input[type="button"], input[type="submit"], input[type="image"]';
+let lastButton;
 
-// for polyfill use only require
-polyfillRequire('event-submitter-polyfill');
+document.addEventListener(
+    'click',
+    (event) => {
+        lastButton = event.target?.closest(buttonsSelector);
+    },
+    true
+);
+
+document.addEventListener(
+    'submit',
+    (event) => {
+        if (lastButton && event.submitter) return;
+        Object.defineProperty(Object.getPrototypeOf(event), 'submitter', {
+            configurable: true,
+            enumerable: true,
+            get() {
+                const form = this.target;
+                return (
+                    [document.activeElement, lastButton].find((control) => {
+                        return control && control.matches(buttonsSelector) && form === control.form;
+                    }) || null
+                );
+            },
+        });
+    },
+    true
+);
+
+document.addEventListener('submit', () => {
+    lastButton = undefined;
+});
