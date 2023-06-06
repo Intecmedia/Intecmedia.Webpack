17fe826c6e73a2a0970b27a95f52dd63653d95d1
dima p <dkrnl@yandex.ru>
2023-06-06 10:11:30 +0700

    .

--- source/js/utils/is-async.js
+++ source/js/utils/is-async.js
@@ -1,5 +1,5 @@
 export default function isAsync(obj) {
-    if ((obj && obj.constructor.name === 'AsyncFunction') || ('then' in obj && typeof obj.then === 'function')) {
+    if ((obj && obj.constructor.name === 'AsyncFunction') || (obj && 'then' in obj && typeof obj.then === 'function')) {
         return true;
     }
     return false;
