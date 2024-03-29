2b97c1bb91119100cc87fbd9ed078d808b34cef9
dima p. <dkrnl@yandex.ru>
2024-03-29 22:25:21 +0700

    .

--- svgo.no-data-url.js
+++ svgo.no-data-url.js
@@ -6,16 +6,14 @@ exports.active = true;
 exports.description = 'Not allowed data URL(<image xlink:href="data:...">). Please use files instead.';
 exports.params = {};
 
-exports.fn = (root, options, extra) => {
-    return {
-        element: {
-            enter: (node) => {
-                if (!(node.type === 'element' && node.name === 'image')) return;
-                const href = node.attributes['xlink:href'] || node.attributes.href;
-                if (href !== undefined && DATA_URL_PATTERN.test(href)) {
-                    throw new Error(`In ${JSON.stringify(extra.path)} -- ${exports.description}`);
-                }
-            },
+exports.fn = (root, options, extra) => ({
+    element: {
+        enter: (node) => {
+            if (!(node.type === 'element' && node.name === 'image')) return;
+            const href = node.attributes['xlink:href'] || node.attributes.href;
+            if (href !== undefined && DATA_URL_PATTERN.test(href)) {
+                throw new Error(`In ${JSON.stringify(extra.path)} -- ${exports.description}`);
+            }
         },
-    };
-};
+    },
+});
