237995b976df45bfb39c871f7b473066eda2a940
dima p. <dkrnl@yandex.ru>
2024-03-29 22:23:29 +0700

    fix: svgo plugin api

--- svgo.no-data-url.js
+++ svgo.no-data-url.js
@@ -1,20 +1,21 @@
-exports.name = 'noDataURL';
-
-exports.type = 'perItem';
-
-exports.active = true;
-
-exports.description = 'Not allowed data URL(<image xlink:href="data:...">). Please use files instead.';
-
-exports.params = {};
-
 const DATA_URL_PATTERN = /^data:image\/[^,;]+(;charset=[^;,]*)?(;base64)?,/;
 
-exports.fn = function noDataURL(item, params, extra) {
-    if (!(item.type === 'element' && item.name === 'image')) return;
+exports.name = 'noDataURL';
+exports.type = 'perItem';
+exports.active = true;
+exports.description = 'Not allowed data URL(<image xlink:href="data:...">). Please use files instead.';
+exports.params = {};
 
-    const href = item.attr('xlink:href') || item.attr('href');
-    if (href !== undefined && DATA_URL_PATTERN.test(href.value)) {
-        throw new Error(`In ${JSON.stringify(extra.path)} -- ${exports.description}`);
-    }
+exports.fn = (root, options, extra) => {
+    return {
+        element: {
+            enter: (node) => {
+                if (!(node.type === 'element' && node.name === 'image')) return;
+                const href = node.attributes['xlink:href'] || node.attributes.href;
+                if (href !== undefined && DATA_URL_PATTERN.test(href)) {
+                    throw new Error(`In ${JSON.stringify(extra.path)} -- ${exports.description}`);
+                }
+            },
+        },
+    };
 };
