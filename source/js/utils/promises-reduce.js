023cf88fc6ec1169a537a4b01f5d687d4b291fc5
dima p. <dkrnl@yandex.ru>
2024-03-30 18:43:32 +0700

    .

--- source/js/utils/promises-reduce.js
+++ source/js/utils/promises-reduce.js
@@ -1,5 +1,5 @@
-export function promisesReduce(promisesList, callbackFn) {
-    return promisesList.reduce((promise, index) => promise.then(() => callbackFn(index)), Promise.resolve());
+export function promisesReduce(promises, reducer) {
+    return promises.reduce((promise, index) => promise.then(() => reducer(index)), Promise.resolve());
 }
 
 export default promisesReduce;
