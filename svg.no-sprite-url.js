1a66b778840c2b7426597b2b6d2094a6a17e4145
dima p. <dkrnl@yandex.ru>
2024-05-08 13:55:35 +0700

    .

--- svg.no-sprite-url.js
+++ svg.no-sprite-url.js
@@ -9,18 +9,18 @@ Chromium issue: https://code.google.com/p/chromium/issues/detail?id=109212
 Safari issue: https://bugs.webkit.org/show_bug.cgi?id=105904
 */
 
-const URL_PATTERN = /<[^>]+="url\([^\)]+\)"[^>]*>/i;
+const URL_PATTERN = /<[^>]+="url\([^)]+\)"[^>]*>/gi;
 
 module.exports = function noSpriteURL(filepath) {
     const relpath = UTILS.slash(path.relative(__dirname, path.normalize(filepath)));
     const content = fs.readFileSync(filepath).toString();
 
     if (URL_PATTERN.test(content)) {
-        const [tag] = content.match(URL_PATTERN);
+        const matches = [...content.match(URL_PATTERN)];
 
         throw new Error(
             [
-                `[svg-sprite] external content \`${tag}\` not allowed in: ${relpath}`,
+                `[svg-sprite] external content \`${matches.join('\`, \`')}\` not allowed in: ${relpath}`,
                 'Chrome issue: https://code.google.com/p/chromium/issues/detail?id=109212',
                 'Safari issue: https://bugs.webkit.org/show_bug.cgi?id=105904',
             ].join('\n')
