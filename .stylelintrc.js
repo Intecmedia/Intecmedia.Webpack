5a5f076bbd44f1a8636b244e96b2f1a5a081dd46
dima p. <dkrnl@yandex.ru>
2024-07-18 08:36:13 +0700

    fix: sass warnings

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -114,7 +114,7 @@ module.exports = deepMerge({}, sharedConfig, {
                 'ignoreTypes': ['/^(html|body|svg|picture|img|iframe|video|option|optgroup|canvas)\\.?/'],
             },
         ],
-        'selector-nested-pattern': ['^(&--|&__|&::|&:|&\\.|&\\[|>|~|\\+|\\w+|\\.)'],
+        'selector-nested-pattern': ['^(&--|&__|&::|&:|&\\.|&\\[|>|&\\s*|~|\\+|\\w+|\\.)'],
         'selector-no-qualifying-type': [
             true,
             {
