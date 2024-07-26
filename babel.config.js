a868bc9d53e5d77a5e6f6cce12cda7f292d40f44
dima p. <dkrnl@yandex.ru>
2024-07-26 10:33:54 +0700

    feat: add `'@babel/plugin-transform-nullish-coalescing-operator`

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = {
         ['@babel/plugin-transform-logical-assignment-operators', {}],
         ['@babel/plugin-syntax-top-level-await', {}],
         ['@babel/plugin-proposal-optional-chaining-assign', { version: '2023-07' }],
+        ['@babel/plugin-transform-nullish-coalescing-operator', {}],
     ],
     presets: [
         [
