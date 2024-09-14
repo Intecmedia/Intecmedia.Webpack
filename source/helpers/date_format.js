9a3246207e788dc3e31b894dc65c7f320432b731
dima p. <dkrnl@yandex.ru>
2024-09-14 11:31:11 +0700

    fix: date format default args

--- source/helpers/date_format.js
+++ source/helpers/date_format.js
@@ -6,7 +6,7 @@ const dataFns = require('date-fns');
  * @param {string} format - date format
  * @returns {string} - date formated
  */
-function helperDateFormat(date, format) {
-    return dataFns.format(date ? new Date(date) : new Date(), format);
+function helperDateFormat(date = null, format = null) {
+    return dataFns.format(date ? new Date(date) : new Date(), format || 'yyyy-MM-dd');
 }
 module.exports = helperDateFormat;
