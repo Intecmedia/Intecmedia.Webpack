99b40a40492525588c3411a9f0732a3fc2ad6232
dima p. <dkrnl@yandex.ru>
2024-09-19 11:57:49 +0700

    .

--- resolve.config.js
+++ resolve.config.js
@@ -7,6 +7,8 @@ module.exports = {
             '~': path.join(ENV.SOURCE_PATH, 'js'),
             'bootstrap/js/dist': path.join(__dirname, 'node_modules', 'bootstrap/js/src'),
             'gsap': path.join(__dirname, 'node_modules', 'gsap/src'),
+            '@barba/core': path.join(__dirname, 'node_modules', '@barba/core/dist/barba.mjs'),
+            '@barba/prefetch': path.join(__dirname, 'node_modules', '@barba/prefetch/dist/barba-prefetch.mjs'),
             // three: require.resolve('three/src/Three.js'),
         },
     },
