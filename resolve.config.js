b5000c6895bed070347e8975ee28873f5a447d22
dima p. <dkrnl@yandex.ru>
2024-09-19 12:40:24 +0700

    fix: resolve esm

--- resolve.config.js
+++ resolve.config.js
@@ -3,6 +3,7 @@ const ENV = require('./app.env');
 
 module.exports = {
     resolve: {
+        mainFields: ['module', 'main'],
         alias: {
             '~': path.join(ENV.SOURCE_PATH, 'js'),
             'bootstrap/js/dist': path.join(__dirname, 'node_modules', 'bootstrap/js/src'),
