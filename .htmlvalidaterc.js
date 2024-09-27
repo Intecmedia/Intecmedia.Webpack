fb1f03becbc660c8e5916bc1c4850a06a9df6017
dima p. <dkrnl@yandex.ru>
2024-09-27 15:21:35 +0700

    fix: enable `pitcher/img-loading-required` rule

--- .htmlvalidaterc.js
+++ .htmlvalidaterc.js
@@ -84,12 +84,10 @@ module.exports = {
                 'ignore': '.wysiwyg iframe, .counters-body iframe, iframe.ignore-html-validate',
             },
         ],
-        /*
         'pitcher/img-loading-required': ['error', {
             'ignore': '.wysiwyg img, .counters-body img, img.ignore-html-validate',
             'intrinsicsize': true,
         }],
-        */
         'pitcher/img-picture-required': [
             'error',
             {
