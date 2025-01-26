34aa4849062bffa6e593101b048ac3db62915788
dima p. <dkrnl@yandex.ru>
2025-01-26 21:44:30 +0700

    feat: fetch abort controller

--- source/js/utils/fetch-timeout.js
+++ source/js/utils/fetch-timeout.js
@@ -1,8 +1,17 @@
 export default function fetchTimeout(input, init = {}, delay = 5000) {
+    /* eslint-disable-next-line compat/compat -- checked in ternary if */
+    const controller = !('signal' in init) && window.AbortController ? new AbortController() : null;
+    if (controller) {
+        init.signal = controller.signal;
+    }
+
     return Promise.race([
         fetch(input, init),
         new Promise((resolve, reject) => {
             setTimeout(() => {
+                if (controller) {
+                    controller.abort();
+                }
                 reject(new Error(`Timeout error [${delay}]`));
             }, delay);
         }),
