1a66f0fb00b63b7bddc57b18e65996b8bb517797
dima p. <dkrnl@yandex.ru>
2025-01-27 08:17:22 +0700

    fix: fetch timeout leaks

--- source/js/utils/fetch-timeout.js
+++ source/js/utils/fetch-timeout.js
@@ -1,17 +1,32 @@
 export default function fetchTimeout(input, init = {}, delay = 5000) {
-    /* eslint-disable-next-line compat/compat -- checked in ternary if */
-    const controller = !('signal' in init) && window.AbortController ? new AbortController() : null;
-    if (controller) {
+    var controller, timeoutId;
+    if (!('signal' in init) && window.AbortController) {
+        /* eslint-disable-next-line compat/compat -- checked in ternary if */
+        controller = new AbortController();
         init.signal = controller.signal;
     }
-
     return Promise.race([
-        fetch(input, init),
+        fetch(input, init)
+            .then((response) => {
+                if (timeoutId) {
+                    clearTimeout(timeoutId);
+                    timeoutId = null;
+                }
+                return response;
+            })
+            .catch((error) => {
+                if (timeoutId) {
+                    clearTimeout(timeoutId);
+                    timeoutId = null;
+                }
+                throw error;
+            }),
         new Promise((resolve, reject) => {
-            setTimeout(() => {
+            timeoutId = setTimeout(() => {
                 if (controller) {
                     controller.abort();
                 }
+                timeoutId = null;
                 reject(new Error(`Timeout error [${delay}]`));
             }, delay);
         }),
