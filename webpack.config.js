3ae40d120a0c279ee1d0b20cc39fe1ec9314fa1a
dima p. <dkrnl@yandex.ru>
2025-03-31 09:58:24 +0700

    deps: update `webpack-assets-manifest`

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const CompressionPlugin = ENV.PROD && !ENV.DEBUG ? require('compression-webpack-plugin') : () => {};
 const TerserPlugin = ENV.PROD && !ENV.DEBUG ? require('terser-webpack-plugin') : () => {};
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
-const WebpackAssetsManifest = require('webpack-assets-manifest');
+const { WebpackAssetsManifest } = require('webpack-assets-manifest');
 
 const FaviconsPlugin = APP.FAVICONS ? require('./plugin.favicons') : () => {};
 const HtmlBeautifyPlugin = APP.HTML_PRETTY ? require('./plugin.html-beautify') : () => {};
