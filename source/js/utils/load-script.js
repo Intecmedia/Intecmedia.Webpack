b74fa36f91e12484e233ac36c3248e8be4107f59
dima p. <dkrnl@yandex.ru>
2025-10-16 14:27:18 +0700

    feat: loadScript utils

---
+++ source/js/utils/load-script.js
@@ -0,0 +1,21 @@
+export function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const existsScript = document.querySelector(`script[src='${src}']`);
+        if (existsScript instanceof HTMLScriptElement) {
+            existsScript.addEventListener('load', () => resolve());
+            existsScript.addEventListener('error', () => reject());
+
+            return existsScript;
+        }
+
+        const scriptElement = document.createElement('script');
+        scriptElement.onload = resolve;
+        scriptElement.onerror = reject;
+        scriptElement.type = 'text/javascript';
+        scriptElement.src = src;
+        scriptElement.async = true;
+        document.body.appendChild(scriptElement);
+
+        return scriptElement;
+    });
+}
