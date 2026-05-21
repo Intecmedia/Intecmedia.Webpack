2b7077c0a6b29856dc7e3776d4642f714f3f490c
dima p. <dkrnl@yandex.ru>
2026-05-21 13:54:29 +0700

    fix: refactor shuffle helper

--- source/helpers/shuffle.js
+++ source/helpers/shuffle.js
@@ -4,6 +4,11 @@
  * @returns {Array} - randomized array
  */
 function helperShuffle(arr) {
-    return arr.sort(() => Math.random() - 0.5);
+    const result = [...arr];
+    for (let i = result.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [result[i], result[j]] = [result[j], result[i]];
+    }
+    return result;
 }
 module.exports = helperShuffle;
