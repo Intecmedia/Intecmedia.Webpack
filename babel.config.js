21e14970913314157d3421bd1aac9fcfaa630f44
dima p. <dkrnl@yandex.ru>
2026-06-23 14:05:13 +0700

    deps: migrate to `babel@8`

--- babel.config.js
+++ babel.config.js
@@ -4,26 +4,25 @@ module.exports = {
     sourceType: 'unambiguous',
     plugins: [
         ['@babel/plugin-transform-optional-chaining', {}],
-        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-transform-class-properties', {}],
         ['@babel/plugin-transform-logical-assignment-operators', {}],
-        ['@babel/plugin-syntax-top-level-await', {}],
-        ['@babel/plugin-proposal-optional-chaining-assign', { version: '2023-07' }],
         ['@babel/plugin-transform-nullish-coalescing-operator', {}],
+        [
+            'babel-plugin-polyfill-corejs3',
+            {
+                method: 'usage-global',
+                version: require('./package.json').dependencies['core-js'],
+            },
+        ],
     ],
     presets: [
         [
             '@babel/preset-env',
             {
-                corejs: { version: '3', proposals: true },
                 modules: false,
-                useBuiltIns: 'usage',
                 debug: ENV.DEBUG || !ENV.PROD || ENV.ARGV.verbose,
                 targets: { browsers: ENV.BROWSERS },
                 ignoreBrowserslistConfig: true,
-                shippedProposals: true,
-                bugfixes: true,
             },
         ],
         ['@babel/preset-typescript', {}],
