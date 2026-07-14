033340195d678545cdf713428e1f89ea8cc4bac8
dima p. <dkrnl@yandex.ru>
2026-07-14 13:55:48 +0700

    feat: bootstrap form-validation

--- source/js/components/bootstrap-validation.js
+++ source/js/components/bootstrap-validation.js
@@ -7,6 +7,8 @@
 
 import AbstractComponent from '~/components/abstract';
 
+const INPUT_SELECTOR = 'input, textarea, select';
+
 /**
  * Bootstrap forms validation.
  */
@@ -17,6 +19,9 @@ export default class BootstrapValidation extends AbstractComponent {
     constructor(options = {}) {
         super(options);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onInput = this.onInput.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onInvalid = this.onInvalid.bind(this);
     }
 
     /**
@@ -26,10 +31,15 @@ export default class BootstrapValidation extends AbstractComponent {
         console.assert(this.element.tagName === 'FORM', 'Required <form>', this.element);
 
         this.element.addEventListener('submit', this.onSubmit);
+        this.element.addEventListener('reset', this.onReset);
+
+        this.element.addEventListener('input', this.onInput);
+        this.element.addEventListener('change', this.onChange);
+        this.element.addEventListener('invalid', this.onInvalid);
     }
 
     /**
-     * Submit-events handler
+     * Submit-events handler.
      * @param {SubmitEvent} event - submit event
      */
     onSubmit(event) {
@@ -38,7 +48,90 @@ export default class BootstrapValidation extends AbstractComponent {
             event.stopPropagation();
         }
 
+        this.element.querySelectorAll(INPUT_SELECTOR).forEach((input) => {
+            this.validateInput(input);
+        });
+
         this.element.classList.add('was-validated');
+    }
+
+    /**
+     * Reset-events handler.
+     */
+    onReset() {
+        this.element.querySelectorAll(INPUT_SELECTOR).forEach((input) => {
+            this.resetInput(input);
+        });
+
+        this.element.classList.remove('was-validated');
+    }
+
+    /**
+     * Invalid-events handler.
+     * @param {Event} event - invalid event
+     */
+    onInvalid(event) {
+        event.preventDefault();
+
+        const input = event.target.closest(INPUT_SELECTOR);
+        if (!input) return;
+
+        this.validateInput(input);
+    }
+
+    /**
+     * Input-events handler.
+     * @param {Event} event - input event
+     */
+    onInput(event) {
+        const input = event.target.closest(INPUT_SELECTOR);
+        if (!input) return;
+
+        this.validateInput(input);
+    }
+
+    /**
+     * Change-events handler.
+     * @param {Event} event - input event
+     */
+    onChange(event) {
+        const input = event.target.closest(INPUT_SELECTOR);
+        if (!input) return;
+
+        this.validateInput(input);
+    }
+
+    /**
+     * Reset input.
+     * @param {HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement} input - input element
+     */
+    resetInput(input) {
+        input.classList.remove('is-invalid');
+        input.classList.remove('is-valid');
+    }
+
+    /**
+     * Validate input.
+     * @param {HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement} input - input element
+     */
+    validateInput(input) {
+        if (input.validity.valid) {
+            input.classList.add('is-valid');
+            input.classList.remove('is-invalid');
+        } else {
+            input.classList.add('is-invalid');
+            input.classList.remove('is-valid');
+        }
+
+        const invalidMessage =
+            input.nextElementSibling &&
+            (input.nextElementSibling.classList.contains('invalid-feedback') || input.nextElementSibling.classList.contains('invalid-tooltip'))
+                ? input.nextElementSibling
+                : null;
+
+        if (invalidMessage) {
+            invalidMessage.textContent = input.validationMessage;
+        }
     }
 
     /**
@@ -46,5 +139,10 @@ export default class BootstrapValidation extends AbstractComponent {
      */
     destroy() {
         this.element.removeEventListener('submit', this.onSubmit);
+        this.element.removeEventListener('reset', this.onReset);
+
+        this.element.removeEventListener('input', this.onInput);
+        this.element.removeEventListener('change', this.onChange);
+        this.element.removeEventListener('invalid', this.onInvalid);
     }
 }
